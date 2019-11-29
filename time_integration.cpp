2523885f51bebd7ec61851f1b246f7ddd5ae9f2c
Edgar Bonet <bonet@grenoble.cnrs.fr>
2019-11-29 17:42:23 +0100

    BUGFIX: inconsistent use of time step
    In time_integration(), the time step dt was modified in multiple places
    within the code. The value used to increment the physical time could
    then be different from the value used to update the system state.
    
    This commit introduces a new class, TimeStepper, that keeps all the
    logic for updating the time step. Now, in time_integration(), dt is
    updated only in a single place.

--- time_integration.cpp
+++ time_integration.cpp
@@ -3,6 +3,29 @@
 #include "fmm_demag.h"
 #include "time_integration.h"
 
+/** Logic for finding a reasonable time step. */
+class TimeStepper {
+    const double hard_max;  /**< never exceed **/
+    double soft_max;        /**< too large for this specific time step */
+public:
+    /** Create a TimeStepper initialized with the given initial and maximum time steps. */
+    TimeStepper(double initial, double max) : hard_max(max), soft_max(2*initial) {}
+
+    /** Update soft_max to be no larger than max. */
+    void set_soft_limit(double max) {
+        soft_max = std::min(soft_max, max);
+    }
+
+    /** Return a reasonable time step. `stride' is distance to the next
+     * time we want to hit exactly. */
+    double operator()(double stride) {
+        double target_dt = soft_max/2;
+        int steps = ceil(stride / target_dt);
+        soft_max = std::min(soft_max * 1.1, hard_max);
+        return stride / steps;
+    }
+};
+
 int time_integration(Fem &fem,Settings &settings /**< [in] */,LinAlgebra &linAlg /**< [in] */,scal_fmm::fmm &myFMM  /**< [in] */,timing &t_prm)
 {
 fem.DW_z  = 0.0;
@@ -29,7 +52,7 @@ double my_t= t_prm.t;
 int nt_output = 0;  // visible iteration count
 int nt = 0;         // total iteration count
 double t_step = settings.time_step;
-double reachable_dt = dt;
+TimeStepper stepper(dt, t_prm.DTMAX);
 
 // Loop over the visible time steps,
 // i.e. those that will appear on the output file.
@@ -38,6 +61,9 @@ for (double t_target = t_prm.t; t_target <  t_prm.tf+t_step/2; t_target += t_ste
     // Loop over the integration time steps within a visible step.
     while (my_t < t_target)
         {
+        t_prm.dt = dt = stepper(t_target - t_prm.t);
+        bool last_step = (dt == t_target - t_prm.t);
+
         if(settings.verbose)
             {
             std::cout << " ------------------------------\n";
@@ -56,26 +82,20 @@ for (double t_target = t_prm.t; t_target <  t_prm.tf+t_step/2; t_target += t_ste
         fem.vmax = linAlg.get_v_max();
         
         if (err)
-            { std::cout << "err : " << err << std::endl;flag++; dt*= 0.5; t_prm.dt=dt; continue;}
+            {
+            std::cout << "err : " << err << std::endl;
+            flag++;
+            stepper.set_soft_limit(dt);
+            continue;
+            }
 
         double dumax = dt*fem.vmax;
         if(settings.verbose) { std::cout << "\t dumax = " << dumax << ",  vmax = "<< fem.vmax << std::endl; }
         if (dumax < settings.DUMIN) break; 
                 
+        stepper.set_soft_limit(settings.DUMAX / fem.vmax);
         if (dumax > settings.DUMAX)
-            { flag++; dt*= 0.5; t_prm.dt=dt; continue;}
-
-        // Adjust the final steps to exactly reach the target time.
-        bool last_step = false;
-        reachable_dt = dt;
-        if (my_t + dt >= t_target) {
-            dt = t_target - my_t;
-            t_prm.dt = dt;
-            last_step = true;
-        } else if (my_t + dt + t_prm.DTMIN > t_target) {
-            dt = (t_target - my_t) / 2;
-            t_prm.dt = dt;
-        }
+            { flag++; continue;}
 
         myFMM.calc_demag(fem,settings);
            
@@ -105,14 +125,7 @@ for (double t_target = t_prm.t; t_target <  t_prm.tf+t_step/2; t_target += t_ste
                 }
             
             }
-        dt = std::min(1.1*dt, t_prm.DTMAX); 
-        t_prm.dt=dt;
         }//endwhile
-    if (dt < 1.1 * reachable_dt)
-        {
-        dt = std::min(1.1 * reachable_dt, t_prm.DTMAX);
-        t_prm.dt = dt;
-        }
     fem.saver(settings,t_prm,fout,nt_output++);
     }// end for
 
