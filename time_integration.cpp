8909915a7ac10ea740cee9214f92e89a09bb6d78
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2019-11-29 18:57:15 +0100

    better t_prm use in time_integration.cpp

--- time_integration.cpp
+++ time_integration.cpp
@@ -46,11 +46,10 @@ if(settings.evol_header)
     }
 
 int flag  = 0;
-double dt = t_prm.dt;
 int nt_output = 0;  // visible iteration count
 int nt = 0;         // total iteration count
 double t_step = settings.time_step;
-TimeStepper stepper(dt, t_prm.DTMAX);
+TimeStepper stepper(t_prm.dt, t_prm.DTMAX);
 
 // Loop over the visible time steps,
 // i.e. those that will appear on the output file.
@@ -59,17 +58,17 @@ for (double t_target = t_prm.t; t_target <  t_prm.tf+t_step/2; t_target += t_ste
     // Loop over the integration time steps within a visible step.
     while (t_prm.t < t_target)
         {
-        t_prm.dt = dt = stepper(t_target - t_prm.t);
-        bool last_step = (dt == t_target - t_prm.t);
+        t_prm.dt = stepper(t_target - t_prm.t);
+        bool last_step = (t_prm.dt == t_target - t_prm.t);
 
         if(settings.verbose)
             {
             std::cout << " ------------------------------\n";
             if (flag) std::cout << "    t  : same (" << flag << ")";
             else std::cout << "nt_output = " << nt_output << ", nt = " << nt << ", t = " << t_prm.t;
-            std::cout << ", dt = " << dt ;
+            std::cout << ", dt = " << t_prm.dt ;
             }
-        if (dt < t_prm.DTMIN) { fem.reset();break; }
+        if (t_prm.dt < t_prm.DTMIN) { fem.reset();break; }
 
         /* changement de referentiel */
         fem.DW_vz += fem.DW_dir*fem.avg(Nodes::get_v_comp,Pt::IDX_Z)*fem.l.z()/2.;
@@ -84,11 +83,11 @@ for (double t_target = t_prm.t; t_target <  t_prm.tf+t_step/2; t_target += t_ste
             {
             std::cout << "err : " << err << std::endl;
             flag++;
-            stepper.set_soft_limit(dt);
+            stepper.set_soft_limit(t_prm.dt);
             continue;
             }
 
-        double dumax = dt*fem.vmax;
+        double dumax = t_prm.dt*fem.vmax;
         if(settings.verbose) { std::cout << "\t dumax = " << dumax << ",  vmax = "<< fem.vmax << std::endl; }
         if (dumax < settings.DUMIN) break; 
                 
@@ -104,14 +103,14 @@ for (double t_target = t_prm.t; t_target <  t_prm.tf+t_step/2; t_target += t_ste
             { std::cout << "Warning energy increases! : " << fem.evol << std::endl; }
 
         fem.DW_vz0 = fem.DW_vz;/* mise a jour de la vitesse du dernier referentiel et deplacement de paroi */ 
-        fem.DW_z  += fem.DW_vz*dt;
+        fem.DW_z  += fem.DW_vz*t_prm.dt;
         fem.evolution(); nt++; flag=0;
 
         // Prevent rounding errors from making us miss the target.
         if (last_step)
             t_prm.t = t_target;
         else
-            t_prm.t += dt;
+            t_prm.t += t_prm.dt;
 
         if(settings.recenter)
             {
@@ -128,7 +127,7 @@ for (double t_target = t_prm.t; t_target <  t_prm.tf+t_step/2; t_target += t_ste
     fem.saver(settings,t_prm,fout,nt_output++);
     }// end for
 
-if (dt < t_prm.DTMIN) { std::cout << " aborted:  dt < DTMIN"; }
+if (t_prm.dt < t_prm.DTMIN) { std::cout << " aborted:  dt < DTMIN"; }
         
 fout.close();
 return nt;
