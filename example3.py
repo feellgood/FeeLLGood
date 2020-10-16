ff0876a5163f2f2a0d2f732667c24da10e1e348c
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2020-10-16 19:17:43 +0200

    icosahedron mesh generated from example3.py script, gmsh file format 2.2

--- example3.py
+++ example3.py
@@ -11,35 +11,37 @@ import numpy as np
 phi = (1+sqrt(5))/2
 pts = np.array([[phi,1,0], [phi,-1,0], [-phi,1,0], [-phi,-1,0],
 [1,0,phi], [1,0,-phi], [-1,0,phi], [-1,0,-phi],
-[0,phi,1], [0,phi,-1], [0,-phi,1], [0,-phi,-1]])
+[0,phi,1], [0,phi,-1], [0,-phi,1], [0,-phi,-1],
+[0,0,0]] )
 
-zero = np.array( [0,0,0] )
+f_idx = [ [0,8,4], [0,4,1], [0,1,5], [0,5,9], [0,9,8], [8,9,2], [8,2,6], [8,6,4], [6,10,4], [6,3,10], [6,2,3], [10,3,11], [10,11,1], [10,1,4], [1,11,5], [11,3,7], [11,7,5], [3,2,7], [7,9,2],  [5,7,9] ]
 
 r1 = [ pts[0], pts[2], pts[3], pts[1] ] 
 r2 = [ pts[4], pts[5], pts[7], pts[6] ]
 r3 = [ pts[8], pts[10], pts[11], pts[9] ]
 
 triangle3D = []
-triangle3D.append( [pts[0], pts[8], pts[4] ] )
-triangle3D.append( [pts[0], pts[4], pts[1] ] )
-triangle3D.append( [pts[0], pts[1], pts[5] ] )
-triangle3D.append( [pts[0], pts[5], pts[9] ] )
-triangle3D.append( [pts[0], pts[9], pts[8] ] )
-triangle3D.append( [pts[8], pts[9], pts[2] ] )
-triangle3D.append( [pts[8], pts[2], pts[6] ] )
-triangle3D.append( [pts[8], pts[6], pts[4] ] )
-triangle3D.append( [pts[6], pts[10], pts[4] ] )
-triangle3D.append( [pts[6], pts[3], pts[10] ] )
-triangle3D.append( [pts[6], pts[2], pts[3] ] )
-triangle3D.append( [pts[10], pts[3], pts[11]] )
-triangle3D.append( [pts[10], pts[11], pts[1]] )
-triangle3D.append( [pts[10], pts[1], pts[4]] )
-triangle3D.append( [pts[1], pts[11], pts[5]] )
-triangle3D.append( [pts[11], pts[3], pts[7]] )
-triangle3D.append( [pts[11], pts[7], pts[5]] )
-triangle3D.append( [pts[3], pts[2], pts[7]] )
-triangle3D.append( [pts[7], pts[9], pts[2]] )
-triangle3D.append( [pts[5], pts[7], pts[9]] )
+for i in range(0,len(f_idx)):
+	triangle3D.append( [pts[ f_idx[i][0] ],pts[ f_idx[i][1] ],pts[ f_idx[i][2] ]] )
+
+meshFile = open("icosahedron.msh",'w')
+meshFile.write("$MeshFormat\n2.2\t0\t8\n$EndMeshFormat\n")
+meshFile.write("$Nodes\n")
+meshFile.write(str(len(pts))+"\n")
+for i in range(0,len(pts)):
+	meshFile.write( str(i+1)+"\t"+str(pts[i][0])+"\t"+str(pts[i][1])+"\t"+str(pts[i][2]) + "\n" )
+meshFile.write("$EndNodes\n")
+meshFile.write("$Elements\n")
+meshFile.write(str(2*len(f_idx))+"\n")
+for i in range(0,len(f_idx)):
+	meshFile.write( str(i+1)+"\t4\t2\t300\t1\t13\t"+str(1+f_idx[i][0])+"\t"+str(1+f_idx[i][1])+"\t"+str(1+f_idx[i][2]) + "\n" )
+for i in range(0,len(f_idx)):
+	meshFile.write( str(len(f_idx)+i+1)+"\t2\t2\t200\t1\t"+str(1+f_idx[i][0])+"\t"+str(1+f_idx[i][1])+"\t"+str(1+f_idx[i][2]) + "\n" )
+
+meshFile.write("$EndElements\n")
+meshFile.close()
+
+
 
 fig = plt.figure()
 #ax = plt.axes(projection='3d')
@@ -69,9 +71,13 @@ fc = []
 for i in range(1,21):
 	fc.append( (i/20,0,0,0.2) )
 
-#fc.append( (1,1,0,0.4) )
-#fc.append( (0,1,0,0.4) )
-#fc.append( (0,0,1,0.4) )
+triangle3D.append(r1)
+triangle3D.append(r2)
+triangle3D.append(r3)
+
+fc.append( (1,1,0,0.4) )
+fc.append( (0,1,0,0.4) )
+fc.append( (0,0,1,0.4) )
 #print(fc)
 ax.add_collection3d( Poly3DCollection( triangle3D, facecolors=fc, linewidths=1))
 plt.show()
