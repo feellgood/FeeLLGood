36b5d9d29f9143640b7e1f4a3ecf0e711d3a99eb
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2020-11-24 17:49:05 +0100

    improved cuboidMaker and bug correction(surface mesh)

--- cuboidMaker.py
+++ cuboidMaker.py
@@ -1,41 +1,28 @@
 # python -m pip install -U pip
 # sudo python -m pip install -U matplotlib
 
-from math import sqrt
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-
 import numpy as np
 
-nbX = 3
-nbY = 3
-nbZ = 1
-
-x_min = -1
-x_max = 1
-dx = (x_max-x_min)/(nbX)
-y_min = -2
-y_max = 2
-dy = (y_max-y_min)/(nbY)
-z_min = -0.6
-z_max = 0.6
-dz = z_max-z_min
-
 class Cuboid(object):
-    def __init__ (self):
+    def __init__ (self,pt_min,pt_max,nbX,nbY,nbZ):
         idx = 1
+        self.nbX = nbX
+        self.nbY = nbY
+        self.nbZ = nbZ
+        dx = (pt_max[0]-pt_min[0])/self.nbX
+        dy = (pt_max[1]-pt_min[1])/self.nbY
+        dz = (pt_max[2]-pt_min[2])/self.nbZ
         self.pts = []
-        for i in range(0,nbX+1):
-            for j in range(0,nbY+1):
-                for k in range(0,nbZ+1):
-                    self.pts.append([idx,x_min+(i*dx),y_min+(j*dy),z_min+(k*dz)])
+        for i in range(0,self.nbX+1):
+            for j in range(0,self.nbY+1):
+                for k in range(0,self.nbZ+1):
+                    self.pts.append([idx,pt_min[0]+(i*dx),pt_min[1]+(j*dy),pt_min[1]+(k*dz)])
                     idx += 1
         self.Tet = []
         self.Fac = []
-        for i in range(0,nbX):
-            for j in range(0,nbY):
-                for k in range(0,nbZ):
+        for i in range(0,self.nbX):
+            for j in range(0,self.nbY):
+                for k in range(0,self.nbZ):
                     A = self.calc_idx(i,j,k)
                     B = self.calc_idx(i+1,j,k)
                     C = self.calc_idx(i+1,j+1,k)
@@ -51,7 +38,7 @@ class Cuboid(object):
                     self.Tet.append([B,C,G,H])
                     self.Tet.append([F,B,G,H])
 
-                    if (nbZ == 1): 
+                    if (self.nbZ == 1): 
                         self.Fac.append([A,B,D])
                         self.Fac.append([B,C,D])
                         self.Fac.append([E,F,H])
@@ -59,23 +46,23 @@ class Cuboid(object):
                     elif (k == 0):
                         self.Fac.append([A,B,D])
                         self.Fac.append([B,C,D])
-                    elif (k == (nbZ-1)):
+                    elif (k == (self.nbZ-1)):
                         self.Fac.append([E,F,H])
                         self.Fac.append([F,G,H])
 
-                    if (nbX == 1): 
+                    if (self.nbX == 1): 
                         self.Fac.append([A,D,H])
                         self.Fac.append([A,E,H])
                         self.Fac.append([B,C,G])
-                        self.Fac.append([B,F,H])
+                        self.Fac.append([B,F,G])
                     elif (i == 0):
                         self.Fac.append([A,D,H])
                         self.Fac.append([A,E,H])
-                    elif (i == (nbX-1)):
+                    elif (i == (self.nbX-1)):
                         self.Fac.append([B,C,G])
-                        self.Fac.append([B,F,H])
+                        self.Fac.append([B,F,G])
 
-                    if (nbY == 1): 
+                    if (self.nbY == 1): 
                         self.Fac.append([A,B,E])
                         self.Fac.append([B,E,F])
                         self.Fac.append([C,G,H])
@@ -83,7 +70,7 @@ class Cuboid(object):
                     elif (j == 0):
                         self.Fac.append([A,B,E])
                         self.Fac.append([B,E,F])
-                    elif (j == (nbY-1)):
+                    elif (j == (self.nbY-1)):
                         self.Fac.append([C,G,H])
                         self.Fac.append([H,D,C])
 
@@ -94,7 +81,7 @@ class Cuboid(object):
         return str(1+self.Fac[idx][0])+"\t"+str(1+self.Fac[idx][1])+"\t"+str(1+self.Fac[idx][2])
 
     def calc_idx(self,i,j,k):
-        return( (nbZ+1)*(nbY+1)*i +  (nbZ+1)*j + k)
+        return( (self.nbZ+1)*(self.nbY+1)*i +  (self.nbZ+1)*j + k)
 
     def make(self,meshFileName,volRegionName,surfRegionName):
         meshFile = open(meshFileName,'w')
@@ -117,7 +104,7 @@ class Cuboid(object):
         meshFile.close()
         print("mesh file " + meshFileName + " generated.")
 
-rectangle = Cuboid()
+rectangle = Cuboid([-1,-2,-3],[1,2,3],4,4,2)
 rectangle.make("rectangle.msh","300","200")
 
 
