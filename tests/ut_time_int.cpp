b01fefa7d6d0ea84841feabf6dde0c611b0a6178
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2021-01-11 16:24:57 +0100

    unit test on calc_alpha_eff in timing object

--- tests/ut_time_int.cpp
+++ tests/ut_time_int.cpp
@@ -7,6 +7,8 @@
 #include <random>
 
 #include "config.h" // for tolerance UT_TOL macro
+#include "pt3D.h"
+#include "time_integration.h"
 
 BOOST_AUTO_TEST_SUITE(ut_time_int)
 
@@ -31,6 +33,47 @@ BOOST_CHECK(x != 0.0f);
 /*---------------------------------------*/
 /* second lvl tests : pure mathematics   */
 /*---------------------------------------*/
+
+BOOST_AUTO_TEST_CASE(calc_alpha_eff, * boost::unit_test::tolerance(UT_TOL))
+{
+std::random_device rd;
+
+std::mt19937 gen(rd());// random number generator: standard Mersenne twister initialized with seed rd()
+std::uniform_real_distribution<> distrib(0.0,1.0);
+
+double alpha_LLG = distrib(gen);
+
+double X= distrib(gen);
+
+timing prm_t = timing(0.0,1e-8,1e-14,1e-9);
+
+// ref code (with minimal adaptations of MuMag_Integrales.cc of src_Tube_scalfmm_thiaville_ec_mu_oersted_thiele_dyn20180903.tgz )
+double dt = prm_t.get_dt();
+double alpha = alpha_LLG;
+double uHeff = X;
+double r = 0.1;                             
+double M = 2.*alpha*r/dt;                   
+
+double alfa=0.;
+    if (uHeff>0.){
+       if (uHeff>M) alfa=alpha+dt/2.*M;
+       else alfa=alpha+dt/2.*uHeff;
+       }
+    else{
+       if (uHeff<-M) alfa=alpha/(1.+dt/(2.*alpha)*M);
+       else alfa=alpha/(1.-dt/(2.*alpha)*uHeff);
+       }
+
+// end ref code
+
+
+// code to check
+double my_alpha = prm_t.calc_alpha_eff(alpha_LLG,X);
+// end code to check
+
+std::cout << "test that calc_alpha_eff gives correct effective damping parameter relatively to ref source code" << std::endl;
+BOOST_TEST( my_alpha == alfa );
+}
 
 
 BOOST_AUTO_TEST_SUITE_END()
