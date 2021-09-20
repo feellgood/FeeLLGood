aad9794d06549a47ffa9ebcbe831c88365a3364a
Edgar Bonet <bonet@grenoble.cnrs.fr>
2021-09-20 14:26:50 +0200

    CI: Align install-dependencies.sh with the doc
    The command "make -j $job_count linux-g++" used to build ANN is run from
    the top ANN directory instead of the src subdirectory (both work). This
    matches the installation procedure documented in
    http://feellgood.neel.cnrs.fr/install.html .
    
    Also added option -q (quiet) to unzip and improved some comments.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,6 +7,10 @@
 # Actions. It is not intended for end-users, and it contains a few calls
 # to `sudo'. Use it at your own risk.
 
+########################################################################
+# The following few commands are not part of the documented installation
+# procedure.
+
 # Set shell options:
 # -e: exit as soon as a command fails
 # -v: print the lines being executed
@@ -15,7 +19,12 @@ set -ev
 # Number of make jobs to run concurrently.
 job_count=$(getconf _NPROCESSORS_ONLN)
 
-# Install apt packages.
+########################################################################
+# The following should match the documented installation procedure, save
+# for the following options added to some commands:
+# apt-get: -y, make: -j, wget: -nv, unzip: -q
+
+# Install required packages.
 sudo apt-get update -q
 sudo apt-get install -y libboost-dev \
     libboost-system-dev libboost-filesystem-dev libboost-test-dev
@@ -23,21 +32,21 @@ sudo apt-get install -y libboost-dev \
 # Download and build the libraries here.
 cd /tmp
 
-# Install ANN.
+# Download and install ANN.
 wget -nv https://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.tar.gz
 tar xzf ann_1.1.2.tar.gz
-cd ann_1.1.2/src/
+cd ann_1.1.2/
 make -j $job_count linux-g++
-sudo cp ../lib/libANN.a /usr/local/lib/
-sudo cp ../include/ANN/ANN.h /usr/local/include/
-cd ../..
+sudo cp lib/libANN.a /usr/local/lib/
+sudo cp include/ANN/ANN.h /usr/local/include/
+cd ..
 
-# Install exprtk.
+# Download and install exprtk.
 wget -nv http://www.partow.net/downloads/exprtk.zip
-unzip exprtk.zip
+unzip -q exprtk.zip
 sudo cp exprtk/exprtk.hpp /usr/local/include/
 
-# Patch, build and install ScalFMM.
+# Download, patch and install ScalFMM.
 wget -nv https://gitlab.inria.fr/solverstack/ScalFMM/-/archive/V1.5.1/ScalFMM-V1.5.1.tar.gz
 tar xzf ScalFMM-V1.5.1.tar.gz
 cd ScalFMM-V1.5.1/
@@ -51,7 +60,7 @@ make -j $job_count
 sudo make install
 cd ../..
 
-# Install GMM.
+# Download and install GMM.
 wget -nv http://download-mirror.savannah.gnu.org/releases/getfem/stable/gmm-5.4.tar.gz
 tar xzf gmm-5.4.tar.gz
 cd gmm-5.4/
