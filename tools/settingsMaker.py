80bac909bd762abc3ec62a98e34ea0072480a51f
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2022-09-16 19:51:08 +0200

    simplified boundary(Dirichlet) conditions in settingsMaker.py

--- tools/settingsMaker.py
+++ tools/settingsMaker.py
@@ -37,8 +37,8 @@ class Settings(object):
 
     ## \brief define spin_polarized_current
     # uk and beta input parameters defining spin transfert torque
-    def createSTT(self,volRegRef,gamma0,sigma,N0,beta,l_J,l_sf,bc_surf_reg_ref1,bc_val_name1,bc_val1,bc_surf_reg_ref2,bc_val_name2,bc_val2):
-        self.mySets["spin_transfer_torque"] = {"enable": True, "volume_region_reference" : str(volRegRef), "gamma0" : gamma0, "sigma" : sigma,"dens_state" : N0, "beta" : beta,"l_J" : l_J,"l_sf" : l_sf,"boundary_conditions" : {  str(bc_surf_reg_ref1) : { bc_val_name1 : bc_val1 } ,  str(bc_surf_reg_ref2) : { bc_val_name2: bc_val2 } } }
+    def createSTT(self,volRegRef,gamma0,sigma,N0,beta,l_J,l_sf,bc_surf_reg_ref1,bc_val1,bc_surf_reg_ref2,bc,bc_val2):
+        self.mySets["spin_transfer_torque"] = {"enable": True, "volume_region_reference" : str(volRegRef), "gamma0" : gamma0, "sigma" : sigma,"dens_state" : N0, "beta" : beta,"l_J" : l_J,"l_sf" : l_sf,"boundary_conditions" : {  str(bc_surf_reg_ref1) : { "V" : bc_val1 } ,  str(bc_surf_reg_ref2) : { "V" : bc_val2 } } }
 
     ## \brief define recentering
     # direction and threshold input parameters defining recentering
@@ -69,7 +69,7 @@ class Settings(object):
             regions[key] = {}
         return regions[key]
 
-    ## \brief some test to check the validity of the parameters
+    ## \brief some test to check the validity of the STT parameters
     # this method is called before writing the json file, it might also be called from another script for some checking before calling the write method
     def check(self):
         if "spin_transfer_torque" in self.mySets:
@@ -79,18 +79,11 @@ class Settings(object):
             elif len(self.mySets["spin_transfer_torque"]["boundary_conditions"]) == 2 :
                 bc = self.mySets["spin_transfer_torque"]["boundary_conditions"]
                 nbV=0
-                nbJ=0
                 for i in bc.values():
                     if isinstance(i,dict) and "V" in i.keys():
                         nbV += 1
-                    if isinstance(i,dict) and "J" in i.keys():
-                        nbJ += 1
                 if nbV == 2:
                     print("Dirichlet boundary conditions.")
-                elif nbV ==1 and nbJ==1:
-                    print("mixt boundary conditions.")
-                elif nbJ==2:
-                    print("Neumann conditions.")
                 else:
                     print("not supported boundary conditions.")
             else :
