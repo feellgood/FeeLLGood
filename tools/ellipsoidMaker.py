3d80139527266d20f001069191768467e3b8fb80
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2023-01-04 17:27:30 +0100

    ellipsoidMaker.py added to tools

---
+++ tools/ellipsoidMaker.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import math
+import gmsh
+
+import numpy as np
+
+class Ellipsoid(object):
+    def __init__ (self,r1,r2,mesh_size,surfName,volName):
+        """ 
+            geometrical ellipsoid is zero centered, with radius r1 in the plane (Oxy) and r2 along (Oz)
+            gmsh file format 2.2 is used to write the mesh text file  
+        """
+        
+        self.r1 = r1
+        self.r2 = r2
+        self.msh_s = mesh_size
+        self.surfName = surfName
+        self.volName = volName
+    
+    def make(self,meshFileName):
+        """ write ellpipsoid mesh file in gmsh 2.2 text format """
+        
+        gmsh.initialize()
+
+        gmsh.model.add("ellipsoid")
+        
+        sph = gmsh.model.occ.addSphere(0,0,0,self.r1)
+        gmsh.model.occ.dilate([(3,sph)],0,0,0,1,1,self.r2)
+        gmsh.model.occ.synchronize() # we have to sync before calling addPhysicalGroup
+
+        surfaces = gmsh.model.getEntities(dim=2)
+        surface_tag = 200
+        gmsh.model.addPhysicalGroup(surfaces[0][0], [surfaces[0][1]], surface_tag) # first parameter should be 2
+        gmsh.model.setPhysicalName(2,surface_tag,self.surfName)
+
+        volumes = gmsh.model.getEntities(dim=3)
+        volume_tag = 300
+        gmsh.model.addPhysicalGroup(volumes[0][0], [volumes[0][1]], volume_tag) # first parameter should be 3
+        gmsh.model.setPhysicalName(3,volume_tag,self.volName)
+
+        gmsh.model.occ.synchronize() # we have to synchronize before the call to 'generate' to build the mesh
+        gmsh.model.mesh.generate(3) # 3 is the dimension of the mesh
+
+        gmsh.option.set_number("Mesh.MshFileVersion", 2.2) # to force mesh file format to 2.2
+
+        gmsh.write(meshFileName)
+
+        nbNodes = gmsh.option.get_number("Mesh.NbNodes")
+        nbTriangles = gmsh.option.get_number("Mesh.NbTriangles")
+        nbTetra = gmsh.option.get_number("Mesh.NbTetrahedra")
+
+# uncomment next line to see a graphic rendering of the mesh
+        #gmsh.fltk.run()
+                
+        gmsh.finalize()
+        
+        print("mesh file " + meshFileName + " generated, nb nodes =" + str(nbNodes) + " , nb tetra =" +str(nbTetra) )
+
