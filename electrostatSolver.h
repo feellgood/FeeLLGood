032a2ef6595dc05dc2ae07848cb88ce89759a6ad
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2023-01-10 16:20:30 +0100

    modified prepareData in electrostat solver, using avg value of the BC's potential values for Lw , does not seems to change neither solution neither convergence speed

--- electrostatSolver.h
+++ electrostatSolver.h
@@ -21,6 +21,50 @@
 
 #include "spinTransferTorque.h"
 
+
+    /** assemble the matrix K from tet and Ke inputs */
+    inline void assembling_mat(Tetra::Tet const& tet, gmm::dense_matrix <double> const &Ke, write_matrix &K)
+    {
+    for (int ie=0; ie<Tetra::N; ie++)
+        { for (int je=0; je<Tetra::N; je++) { K(tet.ind[ie], tet.ind[je]) += Ke(ie, je); } }
+    }
+    
+    /** assemble the vector L from fac and Le inputs */
+    inline void assembling_vect(Facette::Fac const& fac, std::vector <double> const &Le, write_vector &L)
+    { for (int ie=0; ie<Facette::N; ie++) { L[ fac.ind[ie] ] += Le[ie]; } }
+    
+    
+    /** compute integrales for matrix coefficients,input from tet ; sigma is the region conductivity */
+    void integrales(Tetra::Tet const& tet, double sigma, gmm::dense_matrix <double> &AE)
+    {
+    for (int npi=0; npi<Tetra::NPI; npi++)
+            {
+            double w = tet.weight[npi];
+
+            for (int ie=0; ie<Tetra::N; ie++)
+                {
+                double dai_dx= tet.dadx[ie][npi];
+                double dai_dy= tet.dady[ie][npi];
+                double dai_dz= tet.dadz[ie][npi];
+
+                for (int je=0; je<Tetra::N; je++)
+                    {
+                    double daj_dx= tet.dadx[je][npi];
+                    double daj_dy= tet.dady[je][npi];
+                    double daj_dz= tet.dadz[je][npi];
+                    AE(ie, je) += sigma*(dai_dx*daj_dx + dai_dy*daj_dy + dai_dz*daj_dz)*w;		
+                    }
+                }
+            }
+    }
+    
+    /** compute integrales for vector coefficients, input from facette */
+void integrales(Facette::Fac const& fac,double pot_val, std::vector <double> &BE)
+    {
+    for (int npi=0; npi<Facette::NPI; npi++)
+            { for (int ie=0; ie<Facette::N; ie++) { BE[ie] -= Facette::a[ie][npi]*pot_val*fac.weight(npi); } }
+    }
+
 /** \class electrostatSolver
 this class is containing both data and a solver to compute potential from dirichlet boundary conditions problem for the current density flowing in the sample.
 */
@@ -118,66 +162,31 @@ private:
                       { std::cout << "regName: " << p.first << "\tV :" << p.second << std::endl; } );
         }
         
-    /** assemble the matrix K from tet and Ke inputs */
-    inline void assembling_mat(Tetra::Tet const& tet, gmm::dense_matrix <double> &Ke, write_matrix &K)
-    {
-    for (int ie=0; ie<Tetra::N; ie++)
-        { for (int je=0; je<Tetra::N; je++) { K(tet.ind[ie], tet.ind[je]) += Ke(ie, je); } }
-    }
-    
-    /** assemble the vector L from fac and Le inputs */
-    inline void assembling_vect(Facette::Fac const& fac, std::vector <double> &Le, write_vector &L)
-    { for (int ie=0; ie<Facette::N; ie++) { L[ fac.ind[ie] ] += Le[ie]; } }
-    
-    
-    /** compute integrales for matrix coefficients,input from tet ; sigma is the region conductivity */
-    void integrales(Tetra::Tet const& tet, double sigma, gmm::dense_matrix <double> &AE)
-    {
-    
-    
-        for (int npi=0; npi<Tetra::NPI; npi++)
-            {
-            double w, dai_dx, dai_dy, dai_dz, daj_dx, daj_dy, daj_dz;
-            w = tet.weight[npi];
 
-            for (int ie=0; ie<Tetra::N; ie++)
-                {
-                dai_dx= tet.dadx[ie][npi];  dai_dy= tet.dady[ie][npi];  dai_dz= tet.dadz[ie][npi];
-
-                for (int je=0; je<Tetra::N; je++)
-                    {
-                    daj_dx= tet.dadx[je][npi];  daj_dy= tet.dady[je][npi];  daj_dz= tet.dadz[je][npi];
-                    AE(ie, je) += sigma*(dai_dx*daj_dx + dai_dy*daj_dy + dai_dz*daj_dz)*w;		
-                    }
-                }
-            }
-    }
-    
-    /** compute integrales for vector coefficients, input from facette */
-void integrales(Facette::Fac const& fac,double pot_val, std::vector <double> &BE)
-    {
-    for (int npi=0; npi<Facette::NPI; npi++)
-            { for (int ie=0; ie<Facette::N; ie++) { BE[ie] -= Facette::a[ie][npi]*pot_val*fac.weight(npi); } }
-    }
 
     /** fill matrix and vector to solve potential values on each node */
 void prepareData(write_matrix &Kw, write_vector & Lw)
 {
-for (unsigned int ne=0; ne < msh.tet.size(); ne++){
-    Tetra::Tet const& tet = msh.tet[ne];
-    gmm::dense_matrix <double> K(Tetra::N, Tetra::N);
-    double sigma = p_stt.sigma;
-    integrales(tet,sigma, K);
-    assembling_mat(tet, K, Kw);
-    }
+const double sigma = p_stt.sigma;
 
-for (unsigned int ne=0; ne < msh.fac.size(); ne++){
-    Facette::Fac const& fac = msh.fac[ne];
-    std::vector <double> L(Facette::N);
-    double pot_val =0; // to find in V_values
-    integrales(fac, pot_val, L);     
-    assembling_vect(fac, L, Lw);
-    }    
+std::for_each(msh.tet.begin(),msh.tet.end(), [this,sigma,&Kw]( Tetra::Tet const& tet )
+		{  
+		gmm::dense_matrix <double> K(Tetra::N, Tetra::N);
+		integrales(tet,sigma, K);
+    		assembling_mat(tet, K, Kw);
+		});
+
+
+double pot_val = 0; // we initialize pot_val to the average of the potentials set by the boundary conditions (does not seem to change convergence speed whatever value it is ..)
+std::for_each( p_stt.boundaryCond.begin(),p_stt.boundaryCond.end(), [&pot_val](auto const& it) { pot_val += it.second;} );
+pot_val /= p_stt.boundaryCond.size();
+
+std::for_each(msh.fac.begin(),msh.fac.end(), [this,pot_val,&Lw]( Facette::Fac const& fac )
+		{
+		std::vector <double> L(Facette::N);
+		integrales(fac, pot_val, L);     
+    		assembling_vect(fac, L, Lw);
+		});
 }
 
 /** solver, using biconjugate stabilized gradient, with diagonal preconditionner */
@@ -216,26 +225,6 @@ std::for_each( p_stt.boundaryCond.begin(),p_stt.boundaryCond.end(), [this,&Kw,&L
 			});
 		}
 	} );
-/*
-for (unsigned int ne=0; ne<msh.fac.size(); ne++)
-    {
-    Facette::Fac const& fac = msh.fac[ne];
-    std::map<std::string,double>::iterator it = V_values.find(fac.idxPrm); // might not be idxPrm 
-        
-    if (it != V_values.end() ) 
-        {
-        double V = it->second;
-        for (int ie=0; ie<Facette::N; ie++)
-            {
-            const int i= fac.ind[ie];
-            std::for_each(mat_row(Kw, i).begin(),mat_row(Kw, i).end(), [](std::pair<const long unsigned int, double> & it) { it.second = 0.0; } );
-            Kw(i, i) =  1e9;
-            Lw[i]    =  V*1e9;  
-            Xw[i]    =  V;
-            }
-        }
-    }
-*/
 
 if(verbose) { std::cout << "line weighting..." << std::endl; }
 /* equilibrage des lignes */
