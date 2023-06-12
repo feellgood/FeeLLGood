dba26789306b1c3eecaf13c9c0b9f0d92977876c
Christophe <christophe-thirion>
2023-06-12 16:44:54 +0200

    doxygen link from mainpage to mesh class

--- mesh.h
+++ mesh.h
@@ -2,7 +2,7 @@
 #define mesh_h
 
 /** \file mesh.h
-\brief class mesh, read_mesh, save
+\brief class mesh, readMesh is expecting a mesh file in gmsh text format 2.2, with first order tetraedrons and triangular facettes.
 */
 
 #include <execution>
