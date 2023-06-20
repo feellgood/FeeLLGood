0cefdc7f4a204121a7456d4d7c53b89166ad3676
Edgar Bonet <bonet@grenoble.cnrs.fr>
2023-06-20 11:13:28 +0200

    Unhide the expression parser implementations
    Commit 0cb7f83a9a8d4e306178169857b9eaf575728aa9 introduced the pimpl
    (Pointer to IMPLementation) idiom in order to hide the parser
    implementations from the rest of the code. This was meant to avoid
    exposing the <exprtk.hpp> header to more than one compilation unit, as
    this header makes the compilation extremely slow.
    
    Now that exprtk has been replaced by Duktape, compilation time is not an
    issue. Remove this idiom in order to simplify the code.

--- mag_parser.cpp
+++ mag_parser.cpp
@@ -3,196 +3,110 @@
  */
 
 #include "mag_parser.h"
-#include <duktape.h>
 #include <iostream>
-
-/***********************************************************************
- * Code shared between both parsers.
- */
 
 /* Copy all the contents of the Math object (sqrt, sin, log, PI...) to the global object. */
 static const char js_library[] = "Object.getOwnPropertyNames(Math).forEach("
                                  "    function(name) { globalThis[name] = Math[name]; }"
                                  ");";
 
-/**
- * Generic parser and evaluator, parametrized by the type of the parameter:
- *  - T = double for the field, which is a function of time
- *  - T = Pt::pt3D for the initial magnetization, which is a function of position.
+/*******************************************************************************
+ * VectorParser implementation.
  */
-template<typename T>
-struct VectorParser
-    {
-    VectorParser()
-        {
-        ctx = duk_create_heap_default();
-        duk_int_t err = duk_peval_string(ctx, js_library);
-        die_if_error(err);
-        duk_pop(ctx);  // drop the result of the evaluation
-        }
 
-    /**
-     * Abort with an suitable error message if `err` is an actual error,
-     * in which case the top of the stack is assumed to hold the corresponding Error object.
-     */
-    void die_if_error(duk_int_t err)
+VectorParser::VectorParser()
+    {
+    ctx = duk_create_heap_default();
+    duk_int_t err = duk_peval_string(ctx, js_library);
+    die_if_error(err);
+    duk_pop(ctx);  // drop the result of the evaluation
+    }
+
+void VectorParser::die_if_error(duk_int_t err) const
+    {
+    if (err == DUK_ERR_NONE) return;
+    std::cerr << "Script " << duk_safe_to_string(ctx, -1) << '\n';
+    exit(EXIT_FAILURE);
+    }
+
+void VectorParser::push_function(const std::string &parameters, const std::string &expression)
+    {
+    std::string js_function = "function(" + parameters + ") { return (" + expression + "); }";
+    int stack_size_before = duk_get_top(ctx);
+    duk_int_t err = duk_pcompile_string(ctx, DUK_COMPILE_FUNCTION, js_function.c_str());
+    die_if_error(err);
+    int stack_size_after = duk_get_top(ctx);
+    if (stack_size_after != stack_size_before + 1)
         {
-        if (err == DUK_ERR_NONE) return;
-        std::cerr << "Script " << duk_safe_to_string(ctx, -1) << '\n';
+        std::cerr << "Compilation of " << expression << " damaged the stack.\n";
         exit(EXIT_FAILURE);
         }
-
-    /**
-     * Build a function that takes the given parameters and evaluates the given expression.
-     * Push it to the stack.
-     */
-    void push_function(std::string expression)
-        {
-        std::string js_function = "function(" + js_params + ") { return (" + expression + "); }";
-        int stack_size_before = duk_get_top(ctx);
-        duk_int_t err = duk_pcompile_string(ctx, DUK_COMPILE_FUNCTION, js_function.c_str());
-        die_if_error(err);
-        int stack_size_after = duk_get_top(ctx);
-        if (stack_size_after != stack_size_before + 1)
-            {
-            std::cerr << "Compilation of " << expression << " damaged the stack.\n";
-            exit(EXIT_FAILURE);
-            }
-        }
-
-    /**
-     * Compile the expressions into JavaScript functions.
-     * Leave them on the stack at positions (0, 1, 2).
-     */
-    void set_expressions(const std::string &expr_x, const std::string &expr_y,
-                         const std::string &expr_z)
-        {
-        duk_set_top(ctx, 0);  // clear the stack
-        push_function(expr_x);
-        push_function(expr_y);
-        push_function(expr_z);
-        }
-
-    /**
-     * Dummy method to push the parameter to the stack, will be specialized later.
-     */
-    void push(const T &value) {}
-
-    /**
-     * Get a single vector component. `component_index` is either 0, 1, or 2.
-     */
-    double get_vector_component(const T &param, int component_index)
-        {
-        duk_dup(ctx, component_index);  // copy the function to the top of the stack
-        push(param);
-        duk_int_t err = duk_pcall(ctx, dim);
-        die_if_error(err);
-        double val = duk_get_number(ctx, -1);
-        duk_pop(ctx);
-        return val;
-        }
-
-    /**
-     * Get the vector corresponding to the provided parameter.
-     */
-    Pt::pt3D get_vector(const T &param)
-        {
-        double x = get_vector_component(param, 0);
-        double y = get_vector_component(param, 1);
-        double z = get_vector_component(param, 2);
-        return Pt::pt3D(x, y, z);
-        }
-
-    duk_context *ctx;             /**< Ducktape context holding the functions */
-    static const int dim;         /**< dimension of the parameter (1 or 3) */
-    static std::string js_params; /**< JavaScript parameter list: either "t" or "x,y,z" */
-    };
-
-/*
- * VectorParser template specializations for `double` and `Pt::pt3D`.
- * Hide them from Doxygen, as it does not seem to understand them:
- * it says "warning: no matching class member found for [...]"
- */
-//! @cond Doxygen_Suppress
-
-template<>
-const int VectorParser<double>::dim = 1;
-
-template<>
-std::string VectorParser<double>::js_params = "t";
-
-template<>
-void VectorParser<double>::push(const double &value)
-    {
-    duk_push_number(ctx, value);
     }
 
-template<>
-const int VectorParser<Pt::pt3D>::dim = 3;
-
-template<>
-std::string VectorParser<Pt::pt3D>::js_params = "x,y,z";
-
-template<>
-void VectorParser<Pt::pt3D>::push(const Pt::pt3D &value)
+void VectorParser::set_expressions(const std::string &parameters, const std::string &expr_x,
+                                   const std::string &expr_y, const std::string &expr_z)
     {
-    duk_push_number(ctx, value.x());
-    duk_push_number(ctx, value.y());
-    duk_push_number(ctx, value.z());
+    duk_set_top(ctx, 0);  // clear the stack
+    push_function(parameters, expr_x);
+    push_function(parameters, expr_y);
+    push_function(parameters, expr_z);
     }
 
-//! @endcond
-
-/** ********************************************************************
- * \brief Internal implementation of MagnetizationParser.
- */
-struct MagnetizationParser::Impl3Dprm : VectorParser<Pt::pt3D>
+void VectorParser::prepare_call(int position) const
     {
-    };
+    duk_dup(ctx, position);  // copy the function to the top of the stack
+    }
 
-/*
- * MagnetizationParser is just a proxy for it's internal implementation.
+void VectorParser::push_argument(double value) const { duk_push_number(ctx, value); }
+
+double VectorParser::get_component(int argument_count) const
+    {
+    duk_int_t err = duk_pcall(ctx, argument_count);
+    die_if_error(err);
+    double val = duk_get_number(ctx, -1);
+    duk_pop(ctx);
+    return val;
+    }
+
+/*******************************************************************************
+ * MagnetizationParser implementation.
  */
 
-MagnetizationParser::MagnetizationParser() : pimpl3Dprm(new Impl3Dprm) {}
-
-MagnetizationParser::~MagnetizationParser() = default;
-
-void MagnetizationParser::set_expressions(const std::string &Mx, const std::string &My,
-                                          const std::string &Mz)
+double MagnetizationParser::get_vector_component(const Pt::pt3D &p, int component_index) const
     {
-    pimpl3Dprm->set_expressions(Mx, My, Mz);
+    prepare_call(component_index);
+    push_argument(p.x());
+    push_argument(p.y());
+    push_argument(p.z());
+    return get_component(3);
     }
 
 Pt::pt3D MagnetizationParser::get_magnetization(const Pt::pt3D &p) const
     {
-    Pt::pt3D M = pimpl3Dprm->get_vector(p);
+    double Mx = get_vector_component(p, 0);
+    double My = get_vector_component(p, 1);
+    double Mz = get_vector_component(p, 2);
+    Pt::pt3D M(Mx, My, Mz);
     M.normalize();
     return M;
     }
 
-/** ********************************************************************
- * \brief Internal implementation of TimeDepFieldParser.
- */
-struct TimeDepFieldParser::Impl1Dprm : VectorParser<double>
-    {
-    };
-
-/*
- * TimeDepFieldParser is a proxy for it's internal implementation.
+/*******************************************************************************
+ * TimeDepFieldParser implementation.
  */
 
-TimeDepFieldParser::TimeDepFieldParser() : pimpl1Dprm(new Impl1Dprm) {}
-
-TimeDepFieldParser::~TimeDepFieldParser() = default;
-
-void TimeDepFieldParser::set_expressions(const std::string &Bx, const std::string &By,
-                                         const std::string &Bz)
+double TimeDepFieldParser::get_vector_component(double t, int component_index) const
     {
-    pimpl1Dprm->set_expressions(Bx, By, Bz);
+    prepare_call(component_index);
+    push_argument(t);
+    return get_component(1);
     }
 
-Pt::pt3D TimeDepFieldParser::get_timeDepField(const double t_val) const
+Pt::pt3D TimeDepFieldParser::get_timeDepField(const double t) const
     {
-    return pimpl1Dprm->get_vector(t_val);
+    double Bx = get_vector_component(t, 0);
+    double By = get_vector_component(t, 1);
+    double Bz = get_vector_component(t, 2);
+    Pt::pt3D B(Bx, By, Bz);
+    return B;
     }
--- mag_parser.h
+++ mag_parser.h
@@ -1,70 +1,124 @@
 /*
- * MagnetizationParser: parser for the magnetization expressions.
+ * Parsers/evaluators for the analytic expressions given by the user:
+ *  - MagnetizationParser: parser for the initial magnetization M(x, y, z)
+ *  - TimeDepFieldParser: parser for the applied field B(t)
  */
 
 #ifndef mag_parser_h
 #define mag_parser_h
 
 #include "pt3D.h"
-#include <memory>
+#include <duktape.h>
 #include <string>
 
 /**
- * \brief Parse and evaluate the expressions that give the components of
- * the initial magnetization as a function of the position coordinates
- * (x, y, z).
- *
- * A single parser object is used for the three components of the
- * magnetization because the independent variables (node coordinates)
- * are shared by the three expressions.
- *
- * This class **cannot be copied**, as it holds a `unique_ptr`.
+ * Generic parser and evaluator. This is the parent class of the other two parsers, and holds the
+ * code shared between them. It handles three expressions that compute the three components of a
+ * vector, which depend on unspecified parameters.
  */
-class MagnetizationParser
+class VectorParser
+    {
+protected:
+    VectorParser();
+
+    /**
+     * Compile the expressions of the x, y and z components, which depend on the specified
+     * parameters, into JavaScript functions. Leave the compiled functions on the Duktape stack at
+     * positions (0, 1, 2).
+     */
+    void set_expressions(const std::string &parameters, const std::string &expr_x,
+                         const std::string &expr_y, const std::string &expr_z);
+
+    /**
+     * Prepare the Duktape interpreter to call the function at the given stack position.
+     * Positions (0, 1, 2) correspond to components (x, y, z) of the computed vector.
+     */
+    void prepare_call(int position) const;
+
+    /**
+     * Push a function argument to the Duktape stack.
+     */
+    void push_argument(double value) const;
+
+    /**
+     * Compute a component of the vector. This must be called after prepare_call() and one or more
+     * calls to push_argument(). `argument_count` should match the number of arguments pushed.
+     */
+    double get_component(int argument_count) const;
+
+private:
+    /**
+     * Abort with an suitable error message if `err` is an actual error, in which case the top of
+     * the stack is assumed to hold the corresponding Error object.
+     */
+    void die_if_error(duk_int_t err) const;
+
+    /**
+     * Build a function that takes the given parameters and evaluates the given expression.
+     * Push it to the stack.
+     */
+    void push_function(const std::string &parameters, const std::string &expression);
+
+    /**
+     * Ducktape context holding the internal state of the interpreter.
+     */
+    duk_context *ctx;
+    };
+
+/**
+ * Parse and evaluate the expressions that give the components of the initial magnetization as a
+ * function of the position coordinates (x, y, z).
+ */
+class MagnetizationParser : VectorParser
     {
 public:
-    MagnetizationParser();
-    ~MagnetizationParser();
-
     /**
      * Compile the expressions for the magnetization components.
      */
-    void set_expressions(const std::string &Mx, const std::string &My, const std::string &Mz);
+    void set_expressions(const std::string &Mx, const std::string &My, const std::string &Mz)
+        {
+        VectorParser::set_expressions("x,y,z", Mx, My, Mz);
+        }
 
     /**
-     * Evaluate the magnetization at point `p`. Returns the _normalized_
-     * magnetization. This should only called _after_ defining the
-     * expressions with set_expressions().
+     * Evaluate the magnetization at point `p`. Returns the _normalized_ magnetization. This should
+     * only be called _after_ defining the expressions with set_expressions().
      */
     Pt::pt3D get_magnetization(const Pt::pt3D &p) const;
 
 private:
-    class Impl3Dprm;
-    std::unique_ptr<Impl3Dprm> pimpl3Dprm; /**< Pointer to the internal implementation. */
+    /**
+     * Get a single vector component. `component_index` should be either 0, 1, or 2.
+     */
+    double get_vector_component(const Pt::pt3D &p, int component_index) const;
     };
 
 /**
- * \brief Parse and evaluate the expressions that give the components of
- * the time dependant applied field as a function of time t.
- *
- * This class **cannot be copied**, as it holds a `unique_ptr`.
+ * Parse and evaluate the expressions that give the components of the time-dependent applied field
+ * as a function of time t.
  */
-
-class TimeDepFieldParser
+class TimeDepFieldParser : public VectorParser
     {
 public:
-    TimeDepFieldParser();
-    ~TimeDepFieldParser();
+    /**
+     * Compile the expressions for the field components.
+     */
+    void set_expressions(const std::string &Bx, const std::string &By, const std::string &Bz)
+        {
+        VectorParser::set_expressions("t", Bx, By, Bz);
+        }
 
-    /** setter for the expressions of applied field B */
-    void set_expressions(const std::string &Bx, const std::string &By, const std::string &Bz);
-
-    /** getter for time dependant field value */
-    Pt::pt3D get_timeDepField(const double t_val) const;
+    /**
+     * Evaluate the applied field at time `t`. This should only be called _after_ defining the
+     * expressions with set_expressions().
+     */
+    Pt::pt3D get_timeDepField(const double t) const;
 
 private:
-    class Impl1Dprm;
-    std::unique_ptr<Impl1Dprm> pimpl1Dprm; /**< internal implementation pointer */
+    /**
+     * Get a single vector component. `component_index` should be either 0, 1, or 2.
+     */
+    double get_vector_component(double t, int component_index) const;
     };
 
 #endif /* mag_parser_h */
