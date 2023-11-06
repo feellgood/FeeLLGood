a0818ae1eacaca4841d42b6365fbf8a5226e93ea
Christophe <christophe-thirion>
2023-11-06 19:25:41 +0100

    final_time of the simulation is a parsed optional benchmark parameter

--- ci-tests/benchmark.py
+++ ci-tests/benchmark.py
@@ -9,12 +9,12 @@ import timeit
 from math import log2,floor
 from feellgood.meshMaker import Cylinder
 
-def makeSettings(mesh, surface_name, volume_name, nbThreads):
+def makeSettings(mesh, surface_name, volume_name, nbThreads, final_time):
     settings = {
         "outputs": {
             "file_basename": "benchmark",
             "evol_time_step": 1e-12,
-            "final_time": 1e-10,
+            "final_time": final_time,
             "mag_config_every": False
         },
         "mesh": {
@@ -40,7 +40,7 @@ def task2test(settings):
     val = subprocess.run(["../feellgood", "--seed", "2", "-"], input=json.dumps(settings), text=True)
     return val
 
-def bench(outputFileName, elt_sizes, listNbThreads):
+def bench(outputFileName, elt_sizes, listNbThreads, final_time):
     meshFileName = "cylinder.msh"
     surface_name = "surface"
     volume_name = "volume"
@@ -52,7 +52,7 @@ def bench(outputFileName, elt_sizes, listNbThreads):
             mesh = Cylinder(radius, height, elt_size, surface_name, volume_name)
             mesh.make(meshFileName)
             for nbThreads in listNbThreads:
-                settings = makeSettings(meshFileName, surface_name, volume_name, nbThreads)
+                settings = makeSettings(meshFileName, surface_name, volume_name, nbThreads, final_time)
                 t = timeit.timeit(lambda: task2test(settings), number=1)
                 if nbThreads == listNbThreads[-1]:
                     f.write(str(t) + '\n')
@@ -60,7 +60,7 @@ def bench(outputFileName, elt_sizes, listNbThreads):
                     f.write(str(t) + '\t')
         f.close()
 
-def get_params(default_elt_sizes, default_listNbThreads):
+def get_params(default_elt_sizes, default_listNbThreads, default_final_time):
     import argparse
     description = 'feellgood benchmark'
     epilogue = '''
@@ -72,12 +72,15 @@ def get_params(default_elt_sizes, default_listNbThreads):
                         help='table of mesh sizes', default=default_elt_sizes)
     parser.add_argument('-n','--nbThreads',metavar='listNbThreads',type=int,nargs='+',
                         help='table of number of threads', default=default_listNbThreads)
+    parser.add_argument('-t','--final_time',type=float,help='final physical simulation time in s',
+                        default=default_final_time)
     parser.add_argument('--version',action='version',version= __version__,help='show the version number')
     args = parser.parse_args()
-    return args.sizes, args.nbThreads
+    return args.sizes, args.nbThreads, args.final_time
 
 __version__ = '1.0.0'
 if __name__ == '__main__':
+    default_final_time = 2e-11
     default_elt_sizes = [4.0, 3.5, 3.0, 2.5]
     result = subprocess.run(["nproc"],text=True,capture_output=True)
     maxNbThreads = 2**floor(log2(int(result.stdout)))
@@ -86,6 +89,6 @@ if __name__ == '__main__':
     while nb >= (maxNbThreads//4):
         default_listNbThreads.append(nb)
         nb = nb // 2
-    elt_sizes, listNbThreads = get_params(default_elt_sizes, default_listNbThreads)
+    elt_sizes, listNbThreads, final_time = get_params(default_elt_sizes, default_listNbThreads, default_final_time)
     os.chdir(sys.path[0])
-    bench('benchmark.txt', elt_sizes, listNbThreads)
+    bench('benchmark.txt', elt_sizes, listNbThreads, final_time)
