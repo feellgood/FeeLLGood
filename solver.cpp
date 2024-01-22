2a407f3a5324c53f468ddb396b533f126e874cbc
Christophe <christophe-thirion>
2024-01-22 18:35:56 +0100

    assemblage_mat was chronometered with ILU precond analyze_pattern() and  factorize(), now timing is measured separately

--- solver.cpp
+++ solver.cpp
@@ -12,6 +12,12 @@ int LinAlgebra::solver(timing const &t_prm)
     std::for_each(refMsh->tet.begin(), refMsh->tet.end(),
                       [this,&w_K_TH](Tetra::Tet &my_elem) { my_elem.assemblage_mat(NOD,w_K_TH); } );
     
+    if (settings.verbose)
+        {
+        std::cout << "matrix assembly done in " << counter.millis() << std::endl;
+        counter.reset();
+        }
+
     K.setFromTriplets(w_K_TH.begin(),w_K_TH.end());
     _solver.analyzePattern(K);// numerical values in K are not used
     _solver.factorize(K);
@@ -19,16 +25,13 @@ int LinAlgebra::solver(timing const &t_prm)
     if(_solver.info() != Eigen::Success)
         { std::cout <<"sparse matrix factorize(): decomposition failed.\n";exit(1); }
     else if (settings.verbose)
-        { std::cout << "K factorized.\n"; }
-    L_TH.setZero(2*NOD);
+        { std::cout << "K factorized (ILU precond) done in " << counter.millis() << std::endl; }
 
+    L_TH.setZero(2*NOD);
     std::for_each(refMsh->tet.begin(), refMsh->tet.end(),
                       [this](Tetra::Tet &my_elem) { my_elem.assemblage_vect(NOD,L_TH); } );
     std::for_each(refMsh->fac.begin(), refMsh->fac.end(),
                       [this](Facette::Fac &my_elem) { my_elem.assemblage_vect(NOD,L_TH); } );
-
-    if (settings.verbose)
-        { std::cout << "matrix assembly done in " << counter.millis() << std::endl; }
 
     refMsh->buildInitGuess(X_guess);// gamma0 division handled by function buildInitGuess
 
