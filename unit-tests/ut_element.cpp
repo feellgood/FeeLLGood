4c9b40b1eda154901337fee7735444dac57eb51f
Christophe <christophe-thirion>
2024-01-24 15:44:32 +0100

    added more tests on element class (unit-tests ut_element.cpp), including idx bigger init list than the class is designed for.

--- unit-tests/ut_element.cpp
+++ unit-tests/ut_element.cpp
@@ -9,25 +9,18 @@
 
 BOOST_AUTO_TEST_SUITE(ut_element)
 
-/*---------------------------------------*/
-/* minus one test: check boost is fine   */
-/*---------------------------------------*/
-
-BOOST_AUTO_TEST_CASE(Stupid)
-    {
-    float x = 1.0;
-    BOOST_CHECK(x != 0.0f);
-    }
-
 /*-----------------------------------------------------*/
 /* zero lvl tests : direct elementary member functions */
 /*-----------------------------------------------------*/
+
+const int idxPrmToTest = 42;
 
 BOOST_AUTO_TEST_CASE(Fac_full_constructor)
     {
     std::vector<Nodes::Node> node;
     std::cout << "4 param constructor" << std::endl;
-    Facette::Fac f(node, 0, 0, {0, 0, 0});
+    
+    Facette::Fac f(node, 0, idxPrmToTest, {0, 0, 0});
 
     std::cout << "infos:\t";
     f.infos();
@@ -40,6 +33,10 @@ BOOST_AUTO_TEST_CASE(Fac_full_constructor)
     BOOST_CHECK(f.ind[1] == idx);
     BOOST_CHECK(f.ind[2] == idx);
     BOOST_CHECK((f.getN() == Facette::N) && (f.getNPI() == Facette::NPI));
+    BOOST_CHECK(f.Ms == 0);
+    BOOST_CHECK(f.idxPrm == idxPrmToTest);
+    BOOST_CHECK(f.P.norm() == 0);
+    BOOST_CHECK(f.Lp.norm() == 0);
     }
 
 BOOST_AUTO_TEST_CASE(Tet_constructor)
@@ -47,7 +44,7 @@ BOOST_AUTO_TEST_CASE(Tet_constructor)
     std::cout << "constructor test with empty node vector\n";
     std::vector<Nodes::Node> node(0);
 
-    Tetra::Tet tet(node, 0, {0, 0, 0, 0});
+    Tetra::Tet tet(node, idxPrmToTest, {0, 0, 0, 0});
     std::cout << "infos:\t";
     tet.infos();
     /*
@@ -59,6 +56,25 @@ BOOST_AUTO_TEST_CASE(Tet_constructor)
     BOOST_CHECK(tet.ind[2] == idx);
     BOOST_CHECK(tet.ind[3] == idx);
     BOOST_CHECK((tet.getN() == Tetra::N) && (tet.getNPI() == Tetra::NPI));
+    BOOST_CHECK(tet.Ms == 0);
+    BOOST_CHECK(tet.idxPrm == idxPrmToTest);
+    BOOST_CHECK(tet.P.norm() == 0);
+    BOOST_CHECK(tet.Lp.norm() == 0);
     }
 
+BOOST_AUTO_TEST_CASE(Tet_constructor_with_wrong_init_list)
+    {
+    std::cout << "constructor test with empty node vector\n";
+    std::vector<Nodes::Node> node(0);
+    const int extra = 123;
+    Tetra::Tet tet(node, idxPrmToTest, {0, 0, 0, 0, extra});
+    const int idx = -1;
+    
+    BOOST_CHECK(tet.ind[0] == idx);
+    BOOST_CHECK(tet.ind[1] == idx);
+    BOOST_CHECK(tet.ind[2] == idx);
+    BOOST_CHECK(tet.ind[3] == idx);
+    std::cout << "extra idx= " << tet.ind[4] << std::endl;
+    BOOST_CHECK(tet.ind[4] == extra); // really weird!!
+    }
 BOOST_AUTO_TEST_SUITE_END()
