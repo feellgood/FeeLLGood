eed89940166c57f52ea0b14ad2a32b9ba422634c
Christophe <christophe-thirion>
2024-01-26 19:14:32 +0100

    unit test on demag energy: comparison between calculation through phi potentiel using demagEnergy() methods to direct method using demag field calculation

--- unit-tests/ut_energy.cpp
+++ unit-tests/ut_energy.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE tetraTest
 
 #include <boost/test/unit_test.hpp>
-
+#include <iomanip>
 #include <random>
 
 #include "tetra.h"
@@ -26,7 +26,7 @@ BOOST_AUTO_TEST_CASE(demagEnergy, *boost::unit_test::tolerance(UT_TOL))
     std::uniform_real_distribution<> distrib(0.0, 1.0);
 
     Eigen::Vector3d p0(0, 0, 0), p1(1, 0, 0), p2(0, 1, 0), p3(0, 0, 1), 
-                    u0(0, 0, 0), v0(0, 0, 0), mag(0, 0, 0), v(0, 0, 0);
+                    u0(0, 0, 0), v0(0, 0, 0), mag(1, 0, 0), v(0, 0, 0);
     Eigen::Vector3d zero(0,0,0);
     double phi0(0), phi(0), phiv0(0), phiv(0);
 
@@ -35,11 +35,8 @@ BOOST_AUTO_TEST_CASE(demagEnergy, *boost::unit_test::tolerance(UT_TOL))
     Nodes::Node n2 = {p2,   u0,  v0,    mag,   v, zero, zero, phi0, phi, phiv0, phiv};
     Nodes::Node n3 = {p3,   u0,  v0,    mag,   v, zero, zero, phi0, phi, phiv0, phiv};
 
-    node[0] = n0;
-    node[1] = n1;
-    node[2] = n2;
-    node[3] = n3;
-
+    node = {n0, n1, n2, n3};
+    
     for (int i = 0; i < nbNod; i++)
         {
         node[i].u = rand_vec3d(M_PI * distrib(gen), 2 * M_PI * distrib(gen));
@@ -48,6 +45,9 @@ BOOST_AUTO_TEST_CASE(demagEnergy, *boost::unit_test::tolerance(UT_TOL))
 
     // carefull with indices (starting from 1)
     Tetra::Tet t(node, 0, {1, 2, 3, 4});
+    t.Ms = 1.0;
+    //Tetra::prm p_vol;
+    //p_vol.J = 1.0;
 
     double t_dadx[Tetra::N][Tetra::NPI],t_dady[Tetra::N][Tetra::NPI],t_dadz[Tetra::N][Tetra::NPI];
     
@@ -61,7 +61,7 @@ BOOST_AUTO_TEST_CASE(demagEnergy, *boost::unit_test::tolerance(UT_TOL))
     // ref code (with minimal adaptations of MuMag::energy method in file MuMag_energy.cc of
     // src_Tube_scalfmm_thiaville_ec_mu_oersted_thiele_dyn20180903.tgz )
 
-    double Js=42;
+    double Js = mu0 * t.Ms;
     double u_nod[3][Tetra::N], u[3][Tetra::NPI];
     double negphi_nod[Tetra::N], Hdx[Tetra::NPI], Hdy[Tetra::NPI], Hdz[Tetra::NPI];
     
@@ -93,35 +93,32 @@ BOOST_AUTO_TEST_CASE(demagEnergy, *boost::unit_test::tolerance(UT_TOL))
     t.interpolation(Nodes::get_u, _u, dudx, dudy, dudz);
     t.interpolation(Nodes::get_phi, _phi);
     double result_to_test = t.demagEnergy(dudx, dudy, dudz, _phi);
+    std::cout << "E_demag(vol)= " << result_to_test << std::endl;
     
-    Eigen::Matrix<double,Facette::NPI,1> phi_fa;
-    Eigen::Matrix<double,Nodes::DIM,Facette::NPI> u_fa;
+    const int ia = t.ind[0] + 1;
+    const int ib = t.ind[1] + 1;
+    const int ic = t.ind[2] + 1;
+    const int id = t.ind[3] + 1;
     
-    Facette::Fac fa1(node, nbNod, 0, {1, 3, 2});
-    Facette::Fac fa2(node, nbNod, 0, {1, 3, 4});
-    Facette::Fac fa3(node, nbNod, 0, {1, 4, 2});
-    Facette::Fac fa4(node, nbNod, 0, {2, 3, 4});
+    std::vector<Facette::Fac> fa;
+    fa.push_back( Facette::Fac(node, nbNod, 0, {ia, ic, ib} ));
+    fa.push_back( Facette::Fac(node, nbNod, 0, {ib, ic, id} ));
+    fa.push_back( Facette::Fac(node, nbNod, 0, {ia, id, ic} ));
+    fa.push_back( Facette::Fac(node, nbNod, 0, {ia, ib, id} ));
+    std::for_each(fa.begin(),fa.end(),[&t](Facette::Fac &f){ f.Ms = t.Ms;});
     
-    fa1.interpolation(Nodes::get_u, u_fa);
-    fa1.interpolation(Nodes::get_phi, phi_fa);
-    result_to_test += fa1.demagEnergy(u_fa, phi_fa);
-    
-    fa2.interpolation(Nodes::get_u, u_fa);
-    fa2.interpolation(Nodes::get_phi, phi_fa);
-    result_to_test += fa2.demagEnergy(u_fa, phi_fa);
-    
-    fa3.interpolation(Nodes::get_u, u_fa);
-    fa3.interpolation(Nodes::get_phi, phi_fa);
-    result_to_test += fa3.demagEnergy(u_fa, phi_fa);
-    
-    fa4.interpolation(Nodes::get_u, u_fa);
-    fa4.interpolation(Nodes::get_phi, phi_fa);
-    result_to_test += fa4.demagEnergy(u_fa, phi_fa);
-    
+    std::for_each(fa.begin(),fa.end(), [&result_to_test](Facette::Fac &f)
+        {
+        Eigen::Matrix<double,Facette::NPI,1> phi_fa;
+        Eigen::Matrix<double,Nodes::DIM,Facette::NPI> u_fa;
+        f.interpolation(Nodes::get_u, u_fa);
+        f.interpolation(Nodes::get_phi, phi_fa);
+        result_to_test += f.demagEnergy(u_fa, phi_fa);
+        });
     // end code to test
-    
-    //BOOST_TEST(result_to_test == Edemag);
-    BOOST_TEST(UT_TOL == 0);
+    std::cout << "val ref Edemag= " << std::setprecision(15) << Edemag << std::endl;
+    std::cout << "val to test Edemag= " << result_to_test << std::endl;
+    BOOST_TEST( Nodes::sq(result_to_test - Edemag) == 0.0 );
     }
 
 BOOST_AUTO_TEST_SUITE_END()
