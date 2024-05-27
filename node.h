8064ecbe64660774877901fc72b03cd66a562225
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2024-05-27 14:54:31 +0200

    modified setBasis() method to handle eigen 3.3.9 (Debian 11 Bulleye)

--- node.h
+++ node.h
@@ -75,8 +75,13 @@ struct Node
         // Choose for an initial ep the direction, among (X, Y, Z), which is further away from u0.
         Eigen::Index minIdx;
         d[CURRENT].u.cwiseAbs().minCoeff(&minIdx);
-        ep.setUnit(minIdx);
-
+        #if EIGEN_VERSION_AT_LEAST(3,4,0)
+            ep.setUnit(minIdx);
+        #else
+            if (minIdx == IDX_X) { ep = Eigen::Vector3d::UnitX(); }
+            else if (minIdx == IDX_Y) { ep = Eigen::Vector3d::UnitY(); }
+            else { ep = Eigen::Vector3d::UnitZ(); }
+        #endif
         // Gram-Schmidt orthonormalization of (u0, ep).
         ep -= ep.dot(d[CURRENT].u) * d[CURRENT].u;
         ep.normalize();
