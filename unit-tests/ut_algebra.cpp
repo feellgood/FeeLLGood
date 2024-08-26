1f5cb71da3b19a69653c073a06208f48f80f4086
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2024-08-26 16:44:14 +0200

    added a unit test on some elementary algebra functions

---
+++ unit-tests/ut_algebra.cpp
@@ -0,0 +1,36 @@
+#define BOOST_TEST_MODULE algebraTest
+
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+#include "ut_config.h"  // for tolerance UT_TOL macro
+#include "../algebra/algebra.h"
+
+using namespace algebra;
+
+BOOST_AUTO_TEST_SUITE(ut_algebra)
+
+/**
+test on scaled, dot, norm, sub functions
+*/
+BOOST_AUTO_TEST_CASE(test_scaled, *boost::unit_test::tolerance(UT_TOL))
+    {
+    std::vector<double> x {1,4,-2,sqrt(42)};
+    double alpha(2.0);
+    std::vector<double> y(4);
+    y.assign(x.begin(),x.end()); // y <- x
+    scaled(alpha,y); // y *= alpha
+    
+    double result = dot(y,y); // result = scalar product y with himself
+    std::cout << "dot(y,y)= " << result << std::endl;
+    BOOST_CHECK( result == 252.0 );
+    
+    scaled(1.0/alpha,y); // y *= 1.0/alpha
+    
+    sub(x,y); // y -= x 
+    BOOST_CHECK( norm(y) == 0.0 );
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
