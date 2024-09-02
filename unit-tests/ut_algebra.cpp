23529b1f9c741a3859c5a9496e81dcfa2464edb7
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2024-09-02 16:52:53 +0200

    algebra unit test on gradient conjugate algorithm

--- unit-tests/ut_algebra.cpp
+++ unit-tests/ut_algebra.cpp
@@ -6,6 +6,7 @@
 
 #include "ut_config.h"  // for tolerance UT_TOL macro
 #include "../algebra/algebra.h"
+#include "../algebra/cg.h"
 
 using namespace algebra;
 
@@ -213,6 +214,28 @@ BOOST_AUTO_TEST_CASE(test_w_sparseMat, *boost::unit_test::tolerance(UT_TOL))
     BOOST_CHECK( y[1] == 3.14 );
     BOOST_CHECK( y[2] == 5.0 );
     BOOST_CHECK( y[3] == 42.0 );
+    }
+
+/** test on gradient conjugate algorithm */
+BOOST_AUTO_TEST_CASE(test_cg, *boost::unit_test::tolerance(UT_TOL))
+    {
+    const int N=4;
+    w_sparseMat m(N);
+    BOOST_TEST( m.getDim() == N );
+    m.insert( m_coeff(1,1,3.14) );
+    m.insert( m_coeff(0,0,1.0) );
+    m.insert( m_coeff(2,2,5.0) );
+    m.insert( m_coeff(3,3,42.0) );
+    m.insert( m_coeff(1,3,-10.0) );
+    m.insert( m_coeff(1,3,10.0) );
+    m.insert( m_coeff(0,3,0.5) );
+    r_sparseMat bob(m);
+    std::vector<double> b {1.0,1.0,1.0,1.0};
+    std::vector<double> x(N);
+    iteration algo_it;
+    double res = cg(bob,x,b,algo_it);
+    std::cout << "CG test:\nresidu= " << res << std::endl;
+    BOOST_CHECK(res < 1e-6);// default iteration tol is 1e-8
     }
 
 BOOST_AUTO_TEST_SUITE_END()
