50bdadab6f0d61e57884f9c8f725bd083a4cca72
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2025-04-07 17:49:47 +0200

    restructured parameters input file for stt parameters and its boundary conditions (example5)

--- examples/example5.py
+++ examples/example5.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import subprocess
 import json
@@ -5,23 +6,23 @@ import numpy as np
 
 from feellgood.meshMaker import Cylinder
 
+#nanowire dimensions in nm
+mesh_size = 4
+height = 100
+radius = 20
 
-mesh_size = 0.5
-r = 1
-t=10
-
-
-meshFileName = 'my_beautiful_cylinder.msh'
+meshFileName = "nanowire.msh"
 vol_regName = "bulk"
 surf_regName = "frontier(bulk)"
 
-cyl = Cylinder(r,t,mesh_size,surf_regName,vol_regName)
+cyl = Cylinder(radius, height, mesh_size, surf_regName, vol_regName)
 
 #surfaces for boundary conditions
-bc1_regName = "S_left"
-bc2_regName = "S_right"
-cyl.addEdgeSurf(bc1_regName,bc2_regName)
+surface_base_name = "ground"
+surface_top_name = "top_electrode"
 
+#create cylinder edge surfaces as 2D geometric entities in the mesh, to define boundary conditions
+cyl.addEdgeSurf(surface_base_name,surface_top_name)
 
 cyl.make(meshFileName)
 
@@ -36,30 +37,27 @@ settings = {
     "mesh": {
         "filename": meshFileName,
         "length_unit": 1e-9,
-        "volume_regions": { vol_regName: { "alpha_LLG" : 0.02 } },
-        "surface_regions": { surf_regName: {} }
+        "volume_regions": { vol_regName: { "Ae": 1e-11, "Js": 1, "alpha_LLG" : 0.05 } },
+        "surface_regions": { surf_regName: {}, surface_base_name:{"V":0}, surface_top_name:{"J":1} }
     },
-    "initial_magnetization": ["x", "y", 0],
-    "Bext": [1, 0, -1],
+    "initial_magnetization": [1, 0, 1],
+    "Bext": [0, 0, 0],
     "finite_element_solver": { "nb_threads": 32 },
     "demagnetizing_field_solver": { "nb_threads": 32 },
     "time_integration": {
-        "min(dt)": 5e-14,
-        "max(dt)": 3.5e-13,
-        "max(du)": 0.1
+        "min(dt)": 0.5e-16,
+        "max(dt)": 1e-11
     },
     "spin_transfer_torque" : {
-    "enable": True,
-    "sigma": 1,
-    "dens_state": 42,
-    "beta": 1,
-    "l_J": 1,
-    "l_sf": 1,
-    "V_file": True,
-    "boundary_conditions": { bc1_regName: 1.2345, bc2_regName: -3.14 }
+        "enable": True,
+        "sigma": 1,
+        "dens_state": 42,
+        "beta": 1,
+        "l_J": 1,
+        "l_sf": 1,
+        "V_file": False
     }
 }
-
 
 #with open("rect_stt.json",'w') as outfile:
 #    json.dump(settings,outfile,indent = 4)
