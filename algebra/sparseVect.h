35e8d7bf2f99ffe82337be22df1a1d21c010bb83
Edgar Bonet <bonet@grenoble.cnrs.fr>
2025-05-26 14:33:13 +0200

    w_sparseVect: replace the tree by an std::map
    The write-mode sparse vector w_sparseVect is implemented as an ordered
    binary tree holding v_coeff objects (basically, (index, value) pairs).
    Replace the tree by an std::map mapping the indices to the values.
    According to cppreference.com, the map is likely implemented as a
    self-balanced ordered binary tree which, save for the self-balancing
    property, is not very different from the current implementation.
    However, as it is readily available and reasonably optimized, there is
    no point in reinventing the wheel.
    
    This saves 166 lines of code, without making the program any slower.
    Both versions were benchmarked with a 10,125-node mesh, over five
    repeated runs. The measured execution times in seconds are:
    
                    min    median     max
        -----------------------------------
        tree      47.1084  47.2827  47.6456
        std::map  46.4762  47.2241  47.3953

--- algebra/sparseVect.h
+++ algebra/sparseVect.h
@@ -15,211 +15,6 @@ a read sparse vector is a std::vector of v_coeff, built by its constructor
 
 namespace algebra
 {
-/** elementary node */
-template<typename T>
-struct node_t
-    {
-    T *data;
-    node_t *left;
-    node_t *right;
-    };
-
-/** tree to store sparse vector coefficient */
-template<typename T>
-class Tree
-{
-public:
-    Tree(): root(NULL) {}
-    ~Tree() { destroy_tree(root); }
-
-    void insert(T data);
-    node_t<T> *search(int _i) const;
-    void inorder_print();
-    void postorder_print();
-    void preorder_print();
-    void inorder_insert(std::vector<T> &v);
-
-private:
-    void destroy_tree(node_t<T> *leaf);
-    void insert(T data, node_t<T> *leaf);
-    node_t<T> *search(int _i, node_t<T> *leaf) const;
-    void inorder_print(node_t<T> *leaf);
-    void postorder_print(node_t<T> *leaf);
-    void preorder_print(node_t<T> *leaf);
-    void inorder_insert(node_t<T> *leaf, std::vector<T> &v);
-    node_t<T> *root;
-};
-
-/** recursive template function to delete the tree */
-template<typename T>
-void Tree<T>::destroy_tree(node_t<T> *leaf)
-    {
-    if(leaf != NULL)
-        {
-        destroy_tree(leaf->left);
-        destroy_tree(leaf->right);
-        delete leaf->data;
-        delete leaf;
-        }
-    }
-
-/** leaf inserter */
-template<typename T>
-void Tree<T>::insert(T data, node_t<T> *leaf)
-    {
-    if (data._i == leaf->data->_i)
-        {
-        leaf->data->add(data.getVal());
-        return;
-        }
-
-    if (data._i < leaf->data->_i)
-        {
-        if (leaf->left != NULL) { insert(data, leaf->left); }
-        else
-            {
-            leaf->left = new node_t<T>;
-            if (!leaf->left) exit(1);
-            T* data_ptr = new T(data._i, data.getVal());
-            if (!data_ptr) exit(1);
-            leaf->left->data  = data_ptr;
-            leaf->left->left  = NULL;
-            leaf->left->right = NULL;
-            }
-        return;
-        }
-
-    if (data._i > leaf->data->_i)
-        {
-        if (leaf->right != NULL) { insert(data, leaf->right); }
-        else
-            {
-            leaf->right = new node_t<T>;
-            if (!leaf->right) exit(1);
-            T* data_ptr = new T(data._i, data.getVal());
-            if (!data_ptr) exit(1);
-            leaf->right->data  = data_ptr;
-            leaf->right->right = NULL;
-            leaf->right->left  = NULL;
-            }
-        return;
-        }
-    }
-
-/** coeff inserter */
-template<typename T>
-void Tree<T>::insert(T data)
-    {
-    if(root != NULL)
-        { insert(data, root); }
-    else
-        {
-        root = new node_t<T>;
-        if (!root) exit(1);
-        root->data = new T(data._i, data.getVal());
-        root->left = NULL;
-        root->right = NULL;
-        }
-    }
-
-/** search template function for the first occurence of index _i from position leaf */
-template<typename T>
-node_t<T> *Tree<T>::search(int _i, node_t<T> *leaf) const
-    {
-    if(leaf != NULL)
-        {
-        if(_i == leaf->data->_i)
-            { return leaf; }
-
-        if(_i < leaf->data->_i)
-            { return search(_i, leaf->left); }
-        else
-            { return search(_i, leaf->right); }
-        }
-    else
-        { return NULL; }
-    }
-
-/** search template function for the first occurence of index _i in the whole tree */
-template<typename T>
-node_t<T> *Tree<T>::search(int _i) const { return search(_i, root); }
-
-/** printing function */
-template<typename T>
-void Tree<T>::inorder_print()
-    {
-    inorder_print(root);
-    std::cout << std::endl;
-    }
-
-/** printing function */
-template<typename T>
-void Tree<T>::inorder_print(node_t<T> *leaf)
-    {
-    if(leaf != NULL)
-        {
-        inorder_print(leaf->left);
-        std::cout << "{" << leaf->data->_i<< ":"<<leaf->data->getVal() << "},";
-        inorder_print(leaf->right);
-        }
-    }
-
-/** printing function */
-template<typename T>
-void Tree<T>::postorder_print()
-    {
-    postorder_print(root);
-    std::cout << std::endl;
-    }
-
-/** printing function */
-template<typename T>
-void Tree<T>::postorder_print(node_t<T> *leaf)
-    {
-    if(leaf != NULL)
-        {
-        inorder_print(leaf->left);
-        inorder_print(leaf->right);
-        std::cout << "{" << leaf->data->_i<< ":"<<leaf->data->getVal() << "},";
-        }
-    }
-
-/** printing function */
-template<typename T>
-void Tree<T>::preorder_print()
-    {
-    preorder_print(root);
-    std::cout << std::endl;
-    }
-
-/** printing function */
-template<typename T>
-void Tree<T>::preorder_print(node_t<T> *leaf)
-    {
-    if(leaf != NULL)
-        {
-        std::cout << "{" << leaf->data->_i<< ":"<< leaf->data->getVal() << "},";
-        inorder_print(leaf->left);
-        inorder_print(leaf->right);
-        }
-    }
-
-/** inserter for a bunch of coefficient from position root */
-template<typename T>
-void Tree<T>::inorder_insert(std::vector<T> &v) { inorder_insert(root, v); }
-
-/** inserter for a bunch of coefficient from position leaf */
-template<typename T>
-void Tree<T>::inorder_insert(node_t<T> *leaf, std::vector<T> &v)
-    {
-    if(leaf != NULL)
-        {
-        inorder_insert(leaf->left, v);
-        v_coeff data{(int)(leaf->data->_i), leaf->data->getVal()}; //CT : cast to int to silent narrowing
-        v.push_back(data);
-        inorder_insert(leaf->right, v);
-        }
-    }
 
 /** overloading of << for printing functions */
 template<typename T>
@@ -234,29 +29,36 @@ std::ostream& operator << (std::ostream& os, const std::vector<T>& v)
 
 /**
 \class w_sparseVect
-it is a container for v_coeff, in writing mode, using template<v_coeff> tree
+it is a class for sparse vector in writing mode, using std::map<int,double> as coefficient container
 */
 class w_sparseVect
 {
     friend class r_sparseVect;
 public:
     /** inserter with a coefficient */
-    inline void insert(v_coeff coeff) {tree.insert(coeff);}
+    inline void insert(v_coeff coeff) { coefs[coeff._i] += coeff.getVal(); }
 
     /** return true if the coefficient exists */
-    inline bool exist(const int &idx) const { return (tree.search(idx) != NULL); }
+    inline bool exist(const int &idx) const { return coefs.find(idx) != coefs.end(); }
 
     /** getter for the value of a coefficient of index idx, if several coeffs have the same index then it returns the value of the first occurence */
     inline double getVal(const int idx) const
         {
-        node_t< v_coeff > *node=tree.search(idx);
-        if (node==NULL) return 0.0;
-        return node->data->getVal();
+        auto it = coefs.find(idx);
+        if (it == coefs.end()) return 0;
+        return it->second;
+        }
+
+    /** Insert the coefficients into vector v */
+    void inorder_insert(std::vector<v_coeff> &v)
+        {
+        for (auto it = coefs.begin(); it != coefs.end(); ++it)
+            v.push_back(v_coeff{it->first, it->second});
         }
 
 private:
     /** coeffs container */
-    Tree< v_coeff > tree;
+    std::map<int, double> coefs;
 }; // end class w_sparseVect
 
 
@@ -283,12 +85,12 @@ public:
     inline void collect(w_sparseVect &v)
         {
         clear();
-        v.tree.inorder_insert(*this);
+        v.inorder_insert(*this);
         }
 
     /** getter for the value of a coefficient of index idx
     if several coeffs have the same index then it returns the value of the first occurence
-    return zero if coeffciet of index idx does not exist
+    return zero if coefficient of index idx does not exist
      */
     inline double getVal(const int idx) const
         {
