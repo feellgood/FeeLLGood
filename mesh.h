feellgood/FeeLLGood

commit 88bf0fee4bc946588d38454312490aaed681ce1e

Let each volume region store its volume

mesh.h
@@ -28,7 +28,7 @@ class mesh
 public:
     /** constructor : read mesh file, reorder indices and computes some values related to the mesh :
      center and length along coordinates,full volume */
-    inline mesh(Settings const &mySets /**< [in] */)
+    inline mesh(Settings &mySets /**< [in] */)
         : paramTetra(mySets.paramTetra)
         {
         readMesh(mySets);
@@ -67,8 +67,14 @@ public:
             }
         sortNodes(long_axis);
 
-        vol = std::transform_reduce(EXEC_POL, tet.begin(), tet.end(), 0.0, std::plus{},
-                                    [](Tetra::Tet const &te) { return te.calc_vol(); });
+        // Compute the per-region volumes and the total volume.
+        std::for_each(EXEC_POL, tet.begin(), tet.end(),
+                [this](Tetra::Tet const &te)
+                    {
+                    paramTetra[te.idxPrm].volume += te.calc_vol();
+                    });
+        vol = std::transform_reduce(paramTetra.begin(), paramTetra.end(), 0.0,
+                std::plus<>(), [](const Tetra::prm &region){ return region.volume; });
         }
 
     /** return number of nodes  */
@@ -138,7 +144,7 @@ public:
     std::vector<Tetra::Tet> tet;
 
     /** Reference to the volume regions in Settings. */
-    const std::vector<Tetra::prm> &paramTetra;
+    std::vector<Tetra::prm> &paramTetra;
 
     /** read a solution from a file (tsv formated) and initialize fem struct to restart computation
      * from that distribution, return time

fem.h
@@ -48,7 +48,7 @@ class Fem
     {
 public:
     /** constructor: call mesh constructor, initialize pts,kdtree and many inner variables */
-    inline Fem(Settings const &mySets, timing &t_prm) : msh(mySets)
+    inline Fem(Settings &mySets, timing &t_prm) : msh(mySets)
         {
         vmax = 0.0;
         std::fill(E.begin(),E.end(),0);

tetra.h
@@ -75,6 +75,8 @@ struct prm
     Eigen::Vector3d ez; /**< unit vector3 (for cubic anisotropy) */
     STT p_STT;   /**< spin transfert torque (thiaville STT) parameters */
 
+    double volume = 0;  /**< total volume of the region */
+
     /** print the struct parameters */
     inline void infos()
         {

commit 05e88ce6fcbf46fddf8bbca373ca201157d72243

BUGFIX: unparallelize volume computation
The volumes of the sample regions are computed by a parallel loop over
the tetrahedrons, where each iteration adds a contribution to the volume
of the relevant region. Each region volume is thus a piece of data
accessed read/write by as many iteration as there are tetrahedrons in
that region, which is a data race.

Instead of parallelizing over the tetrahedrons, we could parallelize
over the regions. However, since this computation is only done once and
consume negligible time, we won't bother.

Fix the data race by unparallelizing the loop.

Fixes: 88bf0fee4b "Let each volume region store its volume".

mesh.h
@@ -69,7 +69,7 @@ public:
         sortNodes(long_axis);
 
         // Compute the per-region volumes and the total volume.
-        std::for_each(EXEC_POL, tet.begin(), tet.end(),
+        std::for_each(tet.begin(), tet.end(),
                 [this](Tetra::Tet const &te)
                     {
                     paramTetra[te.idxPrm].volume += te.calc_vol();