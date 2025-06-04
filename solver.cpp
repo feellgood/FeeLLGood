693622bb409a595c30167e3fc0c96068f17b373d
Christophe Thirion <christophe.thirion@neel.cnrs.fr>
2025-06-04 19:35:07 +0200

    bicg replaced by bicg_dir to be able to treat problems with magnetic and non-magnetic regions in a problem.

--- solver.cpp
+++ solver.cpp
@@ -24,6 +24,12 @@ int LinAlgebra::solver(timing const &t_prm)
     std::for_each(refMsh->fac.begin(), refMsh->fac.end(),
                       [this](Facette::Fac &my_elem) { my_elem.assemble_vect(L_rhs); } );
 
+    /* RHS forced to zero outside mag material */
+    std::for_each(lvd.begin(),lvd.end(),[this](int i){ L_rhs[i]=0.0; });
+
+    /* to force v=0 on nodes outside magnetic material, diagonal coefficients are forced to 1 */
+    std::for_each(lvd.begin(),lvd.end(),[this](int i){ K.add(i,i,1.0); }); // adding triplet (i,i,1.0) while diag coeff already exists ???? really ???
+    // TODO: we should overload sparseMat constructor to do something like Kr(Kw,lvd);
     if (verbose)
         {
         std::cout << "matrix assembly done in " << counter.millis() << std::endl;
@@ -31,7 +37,7 @@ int LinAlgebra::solver(timing const &t_prm)
         }
 
     buildInitGuess(Xw);// gamma0 division handled by function buildInitGuess
-    algebra::bicg<double>(iter, K, Xw, L_rhs);
+    algebra::bicg_dir<double>(iter, K, Xw, L_rhs, lvd);
 
     if( (iter.status == algebra::ITER_OVERFLOW) || (iter.status == algebra::CANNOT_CONVERGE) || (iter.get_res() > iter.resmax) )
         {
