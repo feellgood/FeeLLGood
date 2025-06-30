8fc83bea87bebbb95b2e3029329142eb1fe11db6
Edgar Bonet <bonet@grenoble.cnrs.fr>
2025-06-30 18:58:50 +0200

    examples: update example2
    Update examples/example2.py in order to scan through the resonance
    frequency of our new ellipsoid mesh.
    
    This example has some weird issues. For instance, outputs.final_time is
    set to 1e-13, yet only data points for t > 1e-8 are taken into account
    (meaning all data points are ignored). Let's then just rewrite the
    entire example and make it closer to example6. Specifically:
    
     * set the applied field to 4 mT (instead of 1 T) in order to stay
       in the linear regime
    
     * save all JSON and evol files, instead of keeping only the last ones,
       as they overwrite each other
    
     * take the amplitude from the final data point, instead of the max from
       the whole time evolution
    
     * while we are at it, compute also the phase of the magnetization
       relative to the applied field.

--- examples/example2.py
+++ examples/example2.py
@@ -1,98 +1,98 @@
 import os
 import sys
+import json
 import subprocess
-from math import sqrt,pi
+from math import sqrt, pi, atan2, remainder
 
-from feellgood.settingsMaker import Settings
+# Main simulation parameters.
+A = 4e-3                 # field amplitude [T]
+start_frequency = 5.3e9  # [Hz]
+stop_frequency = 7.3e9   # [Hz]
+nb_steps_frequency = 21
 
-mySettings = Settings("ellipsoid.msh")
-mySettings.createVolRegion( "ellipsoid_volume" )
-mySettings.createSurfRegion( "ellipsoid_surface" )
+# Output files.
+output_directory = "ferro_resonance"
+output_path = f"{output_directory}/resonance.tsv"
 
-MaxNbThreads = int(subprocess.check_output(["getconf","_NPROCESSORS_ONLN"]))
+# Using many threads is not useful with such a small problem.
+thread_count = 4
 
-mySettings["finite_element_solver"]["nb_threads"] = 4 # MaxNbThreads
-mySettings["finite_element_solver"]["max(iter)"] = 700
+# Simulation settings. Those set to None depend on the frequency. They
+# will be set to their actual values within the frequency scan loop.
+simulation_settings = {
+    "outputs": {
+        "directory": output_directory,
+        "file_basename": None,
+        "evol_time_step": 5e-12,
+        "final_time": 1e-9,
+        "evol_columns": [
+            "t", "<Mx>", "<My>", "<Mz>", "Hx", "Hy", "Hz",
+            "E_ex", "E_demag", "E_zeeman", "E_tot"
+        ],
+        "mag_config_every": False
+    },
+    "mesh": {
+        "filename": "ellipsoid.msh",
+        "length_unit": 1e-9,
+        "volume_regions": { "ellipsoid_volume": { "alpha_LLG": 0.05 } },
+        "surface_regions": { "ellipsoid_surface": {} }
+    },
+    "initial_magnetization": [0, 0, 1],
+    "Bext": None,
+    "demagnetizing_field_solver": { "nb_threads": thread_count },
+    "finite_element_solver": { "max(iter)": 2000 },
+    "time_integration": {
+        "max(du)": 0.01,
+        "min(dt)": 2e-14,
+        "max(dt)": 2e-12
+    }
+}
 
-mySettings["demagnetizing_field_solver"]["nb_threads"] = 4 # MaxNbThreads
-
-mySettings["outputs"]["evol_columns"] = ["t","<Mx>","<My>","<Mz>","<dMx/dt>","<dMy/dt>","<dMz/dt>","Hx","Hy","Hz","E_ex","E_demag","E_zeeman","E_tot"]
-mySettings["outputs"]["mag_config_every"] = 500
-mySettings["outputs"]["directory"] = "test_data_out/"
-mySettings["outputs"]["file_basename"] = "ellipsoid"
-
-mySettings["outputs"]["evol_time_step"] = 5e-16
-mySettings["outputs"]["final_time"] = 1e-13
-
-mySettings["mesh"]["length_unit"] = 1e-10
-
-mySettings["mesh"]["volume_regions"]["ellipsoid_volume"]["alpha_LLG"] = 0.05
-
-mySettings["time_integration"]["min(dt)"] = 5e-18
-mySettings["time_integration"]["max(dt)"] = 2.5e-16
-
-mySettings["time_integration"]["max(du)"] = 0.1
-
-mySettings["initial_magnetization"] = [0.01, 0, 0.99]
-
-if(os.path.exists(mySettings["outputs"]["directory"]) and os.path.isdir(mySettings["outputs"]["directory"]) ):
-    print("directory " + mySettings["outputs"]["directory"] + " already exists.")
+# Create the output directory if needed.
+if (os.path.isdir(output_directory)):
+    print(f"Directory {output_directory} already exists.")
 else:
-    os.system("mkdir " + mySettings["outputs"]["directory"])
+    os.system(f"mkdir {output_directory}")
 
+# Frequency scan.
+output_file = open(output_path, "w")
+output_file.write("#f\tamplitude\tphase\n")
+for i in range(0, nb_steps_frequency):
 
-A = 1.0
+    # Frequency-specific settings.
+    freq = start_frequency \
+        + i * (stop_frequency-start_frequency) / (nb_steps_frequency-1)
+    omega = 2 * pi * freq
+    setting_path = f"{output_directory}/settings_{freq/1e9:.1f}GHz.json"
+    file_basename = f"response_{freq/1e9:.1f}GHz"  # for the .evol file
+    evol_path = f"{output_directory}/{file_basename}.evol"
+    simulation_settings["outputs"]["file_basename"] = file_basename
+    simulation_settings["Bext"] = [
+        f"{A}*cos({omega}*t)", f"{A}*sin({omega}*t)", "0"
+    ]
 
-startFrequency = 4.5e9
-stopFrequency = 5.5e9
-nbStepsFrequency = 200
-currentPath = os.getcwd()
-os.chdir("test_data_out")
-myFile = open("FerroRes.txt","w")
-myFile.write("#f	MinMax\n")
-os.chdir(currentPath)
-for i in range(0,nbStepsFrequency) :
-    freq = startFrequency + i*(stopFrequency - startFrequency)/(nbStepsFrequency-1)
-    omega = 2*pi*freq
-    mySettings["Bext"] = [f"{A}*cos({omega}*t)", f"{A}*sin({omega}*t)", "0"]
-
-    mySettings.write('mySettings.json')
+    # Run the simulation.
+    with open(setting_path, "w") as settings_file:
+        json.dump(simulation_settings, settings_file, indent=4)
+    print(f"\nJSON file {setting_path} generated.")
     sys.stdout.flush()
-    val = subprocess.run(["../feellgood","-v","mySettings.json"])
+    process = subprocess.run(["../feellgood", setting_path])
+    if process.returncode != 0:
+        print("FeeLLGood failed -- aborting.")
+        sys.exit(1)
 
-    amplitudeX = 0
-    amplitudeY = 0
-    if(val.returncode==0):
-        print("FeeLLGood terminated correctly")
-        minMx = 1
-        maxMx = -1
-        minMy = 1
-        maxMy = -1
-        with open(mySettings["outputs"]["directory"] + mySettings["outputs"]["file_basename"] + ".evol","r") as f:
-            for line in f:
-                lastLine = line
-                data = lastLine.split()
-                if (data[0] != "#"):
-                    t = float(data[0])
-                    mx = float(data[1])
-                    my = float(data[2])
-                    #print("values= " + str(t) +";"+ str(mx) +";"+ str(my))
-                    if(t>1e-8):
-                        if(minMx>mx):
-                            minMx = mx
-                        if(maxMx<mx):
-                            maxMx = mx
-
-                        if(minMy>my):
-                            minMy = my
-                        if(maxMy<my):
-                            maxMy = my
-        f.close()
-        amplitudeX = maxMx - minMx
-        amplitudeY = maxMy - minMy
-        print("f=" +str(freq) + " amplitudeX= " + str(amplitudeX) + " amplitudeY= " + str(amplitudeY))
-    myFile.write(str(freq) + "\t" + str(sqrt(amplitudeX**2 + amplitudeY**2)) + "\n")
-    myFile.flush()
-myFile.close()
-sys.exit(0)
-
+    # Get amplitude and phase from the last line of the evolution file.
+    with open(evol_path, "r") as evol_file:
+        for line in evol_file:
+            pass
+    data = line.split()
+    mx = float(data[1])
+    my = float(data[2])
+    Hx = float(data[4])
+    Hy = float(data[5])
+    amplitude = sqrt(mx**2 + my**2)
+    angle_m = atan2(my, mx)
+    angle_H = atan2(Hy, Hx)
+    phase = remainder(angle_m - angle_H, 2 * pi)
+    output_file.write(f"{freq}\t{amplitude}\t{phase}\n")
+    output_file.flush()
