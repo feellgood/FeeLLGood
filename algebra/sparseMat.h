feellgood/FeeLLGood

commit 35f6186cfa224196cb12d2bfb5af2fd8b48702d2

algebra: honor ENABLE_SEQ cmake option
The methods r_sparseMat::mult() and LinAlgebra::solver() use algorithms
from the STL with a parallel execution policy, irrespective of the
ENABLE_SEQ cmake option. Let them honor that option.

Now the ENABLE_SEQ option controls parallelism through all the code save
for the computation of the demagnetizing field, which is controlled by
the setting demagnetizing_field_solver.nb_threads.

algebra/sparseMat.h
@@ -199,7 +199,7 @@ public:
         {
         assert(X.size() == rows.size());
         assert(Y.size() == rows.size());
-        std::transform(std::execution::par, rows.begin(), rows.end(), Y.begin(),
+        std::transform(EXEC_POL, rows.begin(), rows.end(), Y.begin(),
             [&X](const SparseVector &row)
             {
             double val = 0;

solver.cpp
@@ -9,7 +9,7 @@ int LinAlgebra::solver(timing const &t_prm)
     iter.reset();
 
     K.clear();
-    std::for_each(std::execution::par, refMsh->tet.begin(), refMsh->tet.end(),
+    std::for_each(EXEC_POL, refMsh->tet.begin(), refMsh->tet.end(),
                       [this](Tetra::Tet &my_elem) { my_elem.assemblage_mat(K); } );
 
     if (verbose)

commit fcbc11cb637fa5ef80c070445a75e2fdd261390c

BUGFIX: add missing config.h in sparseMat.h
sparseMat.h uses EXEC_POL but does not include config.h, which defines
this macro. This breaks the build of the unit test ut_algebra_bicg.

Fixes: 35f6186cfa algebra: honor ENABLE_SEQ cmake option

algebra/sparseMat.h
@@ -29,6 +29,7 @@ reconstructing the matrix from scratch.
 #include <execution>
 #include <mutex>
 
+#include "config.h"
 #include "algebraCore.h"
 
 namespace algebra