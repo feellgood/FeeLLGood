20a714b4f1f2423fb2569e11469f8434eb26ce10
Edgar Bonet <bonet@grenoble.cnrs.fr>
2025-07-11 16:14:17 +0200

    Properly mask diagonal coefficients in bicg_dir
    Use r_sparseMat::set() instead of r_sparseMat::add(), to ensure the
    masked-out diagonal coefficients are really set to 1.0.

--- solver.cpp
+++ solver.cpp
@@ -24,12 +24,9 @@ int LinAlgebra::solver(timing const &t_prm)
     std::for_each(refMsh->fac.begin(), refMsh->fac.end(),
                       [this](Facette::Fac &my_elem) { my_elem.assemble_vect(L_rhs); } );
 
-    /* RHS forced to zero outside mag material */
-    //std::for_each(lvd.begin(),lvd.end(),[this](int i){ L_rhs[i]=0.0; });
-
-    /* to force v=0 on nodes outside magnetic material, diagonal coefficients are forced to 1 */
-    //std::for_each(lvd.begin(),lvd.end(),[this](int i){ K.add(i,i,1.0); }); // adding triplet (i,i,1.0) while diag coeff already exists ???? really ???
-    // TODO: we should overload sparseMat constructor to do something like Kr(Kw,lvd);
+    /* RHS forced to zero outside mag material defined by mask, corresponding K diagonal coefficients set to 1  */
+    algebra::applyMask(lvd,L_rhs);
+    std::for_each(lvd.begin(),lvd.end(),[this](int i){ K.set(i,i,1.0); });
     if (verbose)
         {
         std::cout << "matrix assembly done in " << counter.millis() << std::endl;
@@ -37,7 +34,7 @@ int LinAlgebra::solver(timing const &t_prm)
         }
 
     buildInitGuess(Xw);// gamma0 division handled by function buildInitGuess
-    algebra::bicg<double>(iter, K, Xw, L_rhs);//algebra::bicg_dir<double>(iter, K, Xw, L_rhs, lvd);
+    algebra::bicg_dir<double>(iter, K, Xw, L_rhs, lvd);
 
     if( (iter.status == algebra::ITER_OVERFLOW) || (iter.status == algebra::CANNOT_CONVERGE) || (iter.get_res() > iter.resmax) )
         {
@@ -48,7 +45,7 @@ int LinAlgebra::solver(timing const &t_prm)
 
     if (verbose)
         { std::cout << "solver: " << iter.infos() <<  " in " << counter.millis() << std::endl; }
-    
+
     double v2max(0.0);
     const double dt = t_prm.get_dt();
     for (int i = 0; i < NOD; i++)
@@ -58,10 +55,8 @@ int LinAlgebra::solver(timing const &t_prm)
         double v2 = Nodes::sq(vp) + Nodes::sq(vq);
         if (v2 > v2max)
             { v2max = v2; }
-        
         refMsh->updateNode(i, vp, vq, dt);//gamma0 multiplication handled in updateNode
         }
     v_max = gamma0*sqrt(v2max);
-    
     return 0;
     }
