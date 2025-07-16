f1aa98eeeccc151489d4f2f8e1327b3c1fdeeb1b
christophe.thirion <christophe.thirion@neel.cnrs.fr>
2025-07-16 15:43:45 +0200

    modified spin acc integrale method for facette, added some doxygen

--- spinAccumulationSolver.cpp
+++ spinAccumulationSolver.cpp
@@ -21,13 +21,6 @@ double spinAcc::getLsd(Tetra::Tet &tet) const
 double spinAcc::getLsf(Tetra::Tet &tet) const
     { return paramTetra[tet.idxPrm].lsf; }
 
-Eigen::Vector3d spinAcc::get_Qn(Facette::Fac const &fac) const
-    {
-    Eigen::Vector3d Pu = paramFacette[fac.idxPrm].Pu;
-    double J  = paramFacette[fac.idxPrm].J;
-    return J*(BOHRS_MUB/CHARGE_ELECTRON)*Pu;
-    }
-
 void spinAcc::prepareExtras(void)
     {
     using namespace Nodes;
@@ -57,9 +50,9 @@ void spinAcc::prepareExtras(void)
                     {
                     Eigen::Vector3d const &_gV = elec.gradV[t.idx].col(npi);
                     Eigen::Vector3d j_grad_u =
-                    -sigma * Eigen::Vector3d(_gV.dot( Eigen::Vector3d(dUdx(Nodes::IDX_X,npi), dUdy(Nodes::IDX_X,npi), dUdz(Nodes::IDX_X,npi))),
-                                             _gV.dot( Eigen::Vector3d(dUdx(Nodes::IDX_Y,npi), dUdy(Nodes::IDX_Y,npi), dUdz(Nodes::IDX_Y,npi))),
-                                             _gV.dot( Eigen::Vector3d(dUdx(Nodes::IDX_Z,npi), dUdy(Nodes::IDX_Z,npi), dUdz(Nodes::IDX_Z,npi))));
+                    -sigma * Eigen::Vector3d(_gV.dot( Eigen::Vector3d(dUdx(IDX_X,npi), dUdy(IDX_X,npi), dUdz(IDX_X,npi))),
+                                             _gV.dot( Eigen::Vector3d(dUdx(IDX_Y,npi), dUdy(IDX_Y,npi), dUdz(IDX_Y,npi))),
+                                             _gV.dot( Eigen::Vector3d(dUdx(IDX_Z,npi), dUdy(IDX_Z,npi), dUdz(IDX_Z,npi))));
 
                     Eigen::Vector3d m = ksi * j_grad_u + U.col(npi).cross(j_grad_u);
                     for (int i = 0; i < N; i++)
@@ -78,7 +71,7 @@ int spinAcc::solve(void)
 
     iter.reset();
     algebra::w_sparseMat Kw(DIM_PROBLEM*NOD);
-    std::vector<double> Lw(DIM_PROBLEM*NOD);
+    std::vector<double> Lw(DIM_PROBLEM*NOD,0);
 
     std::for_each( msh.tet.begin(), msh.tet.end(),[this,&Kw,&Lw](Tetra::Tet &elem)
         {
@@ -205,7 +198,8 @@ void spinAcc::integrales(Facette::Fac &fac, std::vector<double> &BE)
     {
     using namespace Nodes;
     using namespace Facette;
-    Eigen::Vector3d Qn = get_Qn(fac);
+    Eigen::Vector3d Qn = paramFacette[fac.idxPrm].J*(BOHRS_MUB/CHARGE_ELECTRON)*paramFacette[fac.idxPrm].Pu;
+
     for (int npi=0; npi<NPI; npi++)
         {
         double w = fac.weight[npi];
