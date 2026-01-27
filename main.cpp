21d91a7fac0798e2e49964262ac5bdcfa7acdd1d
Edgar Bonet <bonet@grenoble.cnrs.fr>
2026-01-27 12:25:05 +0100

    Display the compile options on starting a simulation
    When starting a simulation, feeLLGood displays its version number
    followed by either of:
    
        parallelism:       disabled
        parallelism:       enabled
    
    depending on the compile option ENABLE_SEQ. Since ONE_GAUSS_POINT is
    just a relevant to the user, let's display both compile options instead.
    In order to keep the printout short, display a comma-separated list of
    enabled options, or "defaults" if no option is enabled. The printed
    message is then one among the following:
    
        compiled with:     defaults
        compiled with:     ONE_GAUSS_POINT
        compiled with:     ENABLE_SEQ
        compiled with:     ONE_GAUSS_POINT, ENABLE_SEQ

--- main.cpp
+++ main.cpp
@@ -204,8 +204,12 @@ int main(int argc, char *argv[])
     prompt();
     if (mySettings.verbose) std::cout << "verbose mode:      on\n";
     std::cout << "feeLLGood version: " << feellgood_version << '\n';
-    if (ENABLE_SEQ) std::cout << "parallelism:       disabled\n";
-    else std::cout << "parallelism:       enabled\n";
+    std::string compile_options;
+    if (ONE_GAUSS_POINT) compile_options += "ONE_GAUSS_POINT, ";
+    if (ENABLE_SEQ) compile_options += "ENABLE_SEQ, ";
+    if (compile_options.empty()) compile_options = "defaults, ";
+    compile_options.resize(compile_options.size() - 2);  // remove trailing ", "
+    std::cout << "compiled with:     " << compile_options << '\n';
     std::cout << "process ID:        " << std::to_string(getpid()) << '\n';
     std::cout << "random seed:       " << random_seed << '\n';
     mySettings.setFileDisplayName(filename == "-" ? "standard input" : filename);
