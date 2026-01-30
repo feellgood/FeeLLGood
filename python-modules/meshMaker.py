0f0071a520bda0b55f684ca4c44747d9fe68644f
christophe.thirion <christophe.thirion@neel.cnrs.fr>
2026-01-30 17:55:01 +0100

    added class Tube to meshMaker python module

--- python-modules/meshMaker.py
+++ python-modules/meshMaker.py
@@ -159,6 +159,90 @@ class Cylinder(object):
         print(f"Generated {meshFileName}: {infos(gmsh.option,'cyl')}")
         gmsh.finalize()
 
+class Tube(object):
+    def __init__ (self,radius1,radius2,length,mesh_size,surfName,volName,fileFormat=4.1,verbose=False):
+        """
+            geometrical Tube is zero centered, with inner radius radius1 and outer radius
+            radius2 and length along (Oz)
+            this tube mesh is generated with python geo
+            by default gmsh file format 4.1 is used to write the mesh text file
+        """
+
+        self.r1 = radius1
+        self.r2 = radius2
+        self.l = length
+        self.msh_s = mesh_size
+        self.surfName = surfName
+        self.volName = volName
+        self.withExtraSurf = False
+        gmsh_init("tube",fileFormat,0,verbose)
+
+    def addEdgeSurf(self,name1,name2):
+        """ optional surfaces : name1 will refer to the base surface, name2 will refer to the translated name1 surface from extrusion """
+        self.n1 = name1
+        self.n2 = name2
+        self.withExtraSurf = True
+
+    def make(self,meshFileName):
+        """ write tube mesh file """
+
+        p_origin = gmsh.model.geo.addPoint(0,0,-0.5*self.l,self.msh_s)
+
+        start_pt = gmsh.model.geo.addPoint(self.r2,0,-0.5*self.l,self.msh_s)
+        interim_pt = gmsh.model.geo.addPoint(0,self.r2,-0.5*self.l,self.msh_s)
+        interim1_pt = gmsh.model.geo.addPoint(-self.r2,0,-0.5*self.l,self.msh_s)
+        interim2_pt = gmsh.model.geo.addPoint(0,-self.r2,-0.5*self.l,self.msh_s)
+
+        start_pt2 = gmsh.model.geo.addPoint(self.r1,0,-0.5*self.l,self.msh_s)
+        interim3_pt = gmsh.model.geo.addPoint(0,-self.r1,-0.5*self.l,self.msh_s)
+        interim4_pt = gmsh.model.geo.addPoint(-self.r1,0,-0.5*self.l,self.msh_s)
+        interim5_pt = gmsh.model.geo.addPoint(0,self.r1,-0.5*self.l,self.msh_s)
+
+        big_circle = gmsh.model.geo.addCircleArc(start_pt,p_origin,interim_pt)
+        big_circle1 = gmsh.model.geo.addCircleArc(interim_pt,p_origin,interim1_pt)
+        big_circle2 = gmsh.model.geo.addCircleArc(interim1_pt,p_origin,interim2_pt)
+        big_circle3 = gmsh.model.geo.addCircleArc(interim2_pt,p_origin,start_pt)
+
+        small_circle = gmsh.model.geo.addCircleArc(start_pt2,p_origin,interim3_pt)
+        small_circle1 = gmsh.model.geo.addCircleArc(interim3_pt,p_origin,interim4_pt)
+        small_circle2 = gmsh.model.geo.addCircleArc(interim4_pt,p_origin,interim5_pt)
+        small_circle3 = gmsh.model.geo.addCircleArc(interim5_pt,p_origin,start_pt2)
+
+        gmsh.model.geo.remove([(p_origin,0)])
+
+        curvedLoop = gmsh.model.geo.addCurveLoop([big_circle,big_circle1,big_circle2,big_circle3]) # curvedLoop is an index (integer)
+        #the following curve defines the hole
+        curvedLoop2 = gmsh.model.geo.addCurveLoop([small_circle,small_circle1,small_circle2,small_circle3])
+
+        surf = gmsh.model.geo.addPlaneSurface([curvedLoop,curvedLoop2]) # surf is an index (integer)
+        out = gmsh.model.geo.extrude([(2,surf)],0,0,self.l) # 2 is the dimension of the object refered by index surf
+        gmsh.model.geo.synchronize() # we have to sync before calling addPhysicalGroup
+
+        surface_tag = 200
+        gmsh.model.addPhysicalGroup(2,[surf, out[0][1], out[2][1], out[3][1], out[4][1], out[5][1],
+        out[6][1], out[7][1], out[8][1], out[9][1] ],surface_tag)
+        gmsh.model.setPhysicalName(2,surface_tag,self.surfName)
+
+        if self.withExtraSurf :
+                surface_tag_left = 201
+                gmsh.model.addPhysicalGroup(2,[surf],surface_tag_left)
+                gmsh.model.setPhysicalName(2,surface_tag_left,self.n1)
+
+                surface_tag_right = 202
+                gmsh.model.addPhysicalGroup(2,[ out[0][1] ],surface_tag_right)
+                gmsh.model.setPhysicalName(2,surface_tag_right,self.n2)
+
+        volume_tag = 300
+        gmsh.model.addPhysicalGroup(3,[out[1][1]],volume_tag)
+        gmsh.model.setPhysicalName(3,volume_tag,self.volName)
+
+        gmsh.model.geo.synchronize() # we have to synchronize before the call to 'generate' to build the mesh
+        gmsh.model.mesh.generate(3) # 3 is the dimension of the mesh
+
+        gmsh.write(meshFileName)
+        print(f"Generated {meshFileName}: {infos(gmsh.option,'tube')}")
+        gmsh.finalize()
+
 class Ellipsoid(object):
     def __init__ (self,r1,r2,mesh_size,surfName,volName,fileFormat=4.1,verbose=False):
         """ 
